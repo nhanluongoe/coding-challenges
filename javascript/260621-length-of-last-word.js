3d17e82732508e747133727e51081db957e24ea5
nhanluongoe <nhanlt18@gmail.com>
2021-06-27 21:26:08 +0700

    Add - length of last word

---
+++ javascript/260621-length-of-last-word.js
@@ -0,0 +1,18 @@
+/**
+ * https://leetcode.com/problems/length-of-last-word/submissions/
+ *
+ */
+
+const lengthOfLastWord = (s) => {
+  return (
+    s
+      .split(/\s+/)
+      .filter((word) => word)
+      .slice(-1)[0]?.length || 0
+  );
+};
+
+console.log(lengthOfLastWord(" "));
+console.log(lengthOfLastWord(" hello world  "));
+console.log(lengthOfLastWord(" abc dee aaa   aaadddd"));
+
