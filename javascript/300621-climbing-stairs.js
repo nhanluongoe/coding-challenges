e3e977e88922d5fd27e4c9342476479ee6a2586b
nhanluongoe <nhanlt18@gmail.com>
2021-06-30 23:26:36 +0700

    Chore: Add another solution

--- javascript/300621-climbing-stairs.js
+++ javascript/300621-climbing-stairs.js
@@ -5,6 +5,7 @@
  * @return {number}
  */
 
+// Memoization-based method
 const memoClimbStairs = (n, cache = {}) => {
   if (n < 3) return n
 
@@ -23,4 +24,22 @@ console.log(climbStairs(2));
 console.log(climbStairs(3));
 console.log(climbStairs(4));
 console.log(climbStairs(25));
+
+// Other method - O(n)
+const climbStairsTwo = (n) => {
+  if (n < 3) return n
+  
+  let cache = {'1': 1, '2': 2}
+
+  for (let i = 3; i <= n; i++) {
+    cache[i] = cache[i - 2] + cache[i - 1]
+  }
+
+  return cache[n]
+}
+
+console.log(climbStairsTwo(2));
+console.log(climbStairsTwo(3));
+console.log(climbStairsTwo(4));
+console.log(climbStairsTwo(25));
 
--- ruby/300621-climbing-stairs.rb
+++ ruby/300621-climbing-stairs.rb
@@ -4,6 +4,7 @@
 # @param {Integer} n
 # @return {Integer}
 
+# Memoization-based method
 class MemoClimbStairs
   def initialize
     @cache = {}
@@ -26,3 +27,21 @@ puts climb_stairs(2).to_s
 puts climb_stairs(3).to_s
 puts climb_stairs(4).to_s
 puts climb_stairs(25).to_s
+
+# Other method - O(n)
+def climb_stairs_two(n)
+  return n if n < 3
+
+  cache = {1 => 1, 2 => 2}
+
+  (3..n).each do |i|
+    cache[i] = cache[i - 2] + cache[i - 1]
+  end
+
+  cache[n]
+end
+
+puts climb_stairs_two(2).to_s
+puts climb_stairs_two(3).to_s
+puts climb_stairs_two(4).to_s
+puts climb_stairs_two(25).to_s
