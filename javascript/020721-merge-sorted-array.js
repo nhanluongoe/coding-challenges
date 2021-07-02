7690f7410f8ac68f2b61d9ce68541cde4b55604f
nhanluongoe <nhanlt18@gmail.com>
2021-07-02 10:44:21 +0700

    Challenge: Add merge sorted array

---
+++ javascript/020721-merge-sorted-array.js
@@ -0,0 +1,26 @@
+/**
+ * https://leetcode.com/problems/merge-sorted-array/
+ *
+ * @param {number[]} nums1
+ * @param {number} m
+ * @param {number[]} nums2
+ * @param {number} n
+ * @return {void} Do not return anything, modify nums1 in-place instead.
+ */
+
+const merge = (nums1, m, nums2, n) => {
+  Array.from(Array(nums1.length - m)).forEach(() => nums1.pop());
+
+  nums2.forEach((elem) => nums1.push(elem));
+
+  nums1.sort((a, b) => a - b)
+};
+
+const nums1 = [1, 2, 3, 0, 0, 0];
+const nums2 = [2, 5, 6];
+const m = 3;
+const n = 3;
+
+merge(nums1, m, nums2, n);
+
+console.log(nums1);
