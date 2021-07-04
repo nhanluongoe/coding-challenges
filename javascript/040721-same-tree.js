afcebfa7ddc53ff63c435cd4bb5d9bdc45578cb7
nhanluongoe <nhanlt18@gmail.com>
2021-07-05 00:33:54 +0700

    challenge: add same-tree

---
+++ javascript/040721-same-tree.js
@@ -0,0 +1,36 @@
+// Definition for a binary tree node.
+function TreeNode(val, left, right) {
+  this.val = val === undefined ? 0 : val;
+  this.left = left === undefined ? null : left;
+  this.right = right === undefined ? null : right;
+}
+
+/**
+ * https://leetcode.com/problems/same-tree/
+ *
+ * @param {TreeNode} p
+ * @param {TreeNode} q
+ * @return {boolean}
+ */
+const isSameTree = (p, q) => {
+  const arrTree1 = preorderTraversal(p);
+  const arrTree2 = preorderTraversal(q);
+
+  return isArrayEqual(arrTree1, arrTree2);
+};
+
+const preorderTraversal = (root) => {
+  if (!root) return [root];
+
+  return [root.val]
+    .concat(preorderTraversal(root.left))
+    .concat(preorderTraversal(root.right));
+};
+
+const isArrayEqual = (arr1, arr2) => {
+  const isSameLength = arr1.length === arr2.length;
+
+  const isSameValue = arr1.every((val, idx) => val === arr2[idx]);
+
+  return isSameLength && isSameValue;
+};
