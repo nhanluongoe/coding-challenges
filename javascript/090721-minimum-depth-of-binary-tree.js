b36894c08d665878a663ebfe283aab86555173de
nhanluongoe <nhanlt18@gmail.com>
2021-07-10 11:35:14 +0700

    refactor: follow eslint rule

--- javascript/090721-minimum-depth-of-binary-tree.js
+++ javascript/090721-minimum-depth-of-binary-tree.js
@@ -7,39 +7,36 @@
  * }
  */
 
+const isLeaf = (node) => node && !(node.left || node.right)
+
+const minDepthHelper = (root) => {
+  if (!root) return Infinity
+
+  if (isLeaf(root)) return 1
+
+  return 1 + Math.min(minDepthHelper(root.left), minDepthHelper(root.right))
+}
+
 /**
  * https://leetcode.com/problems/minimum-depth-of-binary-tree/
  *
  * @param {TreeNode} root
  * @return {number}
  */
-
 const minDepth = (root) => {
-  return !isFinite(minDepthHelper(root)) ? 0 : minDepthHelper(root);
-};
-
-const minDepthHelper = (root) => {
-  if (!root) return Infinity;
-
-  if (isLeaf(root)) return 1;
-
-  return 1 + Math.min(minDepthHelper(root.left), minDepthHelper(root.right));
-};
-
-const isLeaf = (node) => {
-  return node && !(node.left || node.right);
-};
+  return !isFinite(minDepthHelper(root)) ? 0 : minDepthHelper(root)
+}
 
 // shorter solution
 const betterMinDepth = (root) => {
-  if (!root) return 0;
+  if (!root) return 0
 
-  const leftHeight = betterMinDepth(root.left);
-  const rightHeight = betterMinDepth(root.right);
+  const leftHeight = betterMinDepth(root.left)
+  const rightHeight = betterMinDepth(root.right)
 
   if (leftHeight === 0 || rightHeight === 0) {
-    return 1 + leftHeight + rightHeight;
+    return 1 + leftHeight + rightHeight
   }
 
-  return 1 + Math.min(leftHeight, rightHeight);
-};
+  return 1 + Math.min(leftHeight, rightHeight)
+}
