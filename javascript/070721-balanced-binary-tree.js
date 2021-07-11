80946ab6763733d177320a1792b0e445a8ab56da
nhanluongoe <nhanlt18@gmail.com>
2021-07-11 17:13:48 +0700

    refactor: move helper func definition to top

--- javascript/070721-balanced-binary-tree.js
+++ javascript/070721-balanced-binary-tree.js
@@ -1,8 +1,14 @@
 // Definition for a binary tree node.
 function TreeNode(val, left, right) {
-  this.val = val === undefined ? 0 : val;
-  this.left = left === undefined ? null : left;
-  this.right = right === undefined ? null : right;
+  this.val = val === undefined ? 0 : val
+  this.left = left === undefined ? null : left
+  this.right = right === undefined ? null : right
+}
+
+const getHeight = (root) => {
+  if (!root) return 0
+
+  return 1 + Math.max(getHeight(root.left), getHeight(root.right))
 }
 
 /**
@@ -11,22 +17,15 @@ function TreeNode(val, left, right) {
  * @param {TreeNode} root
  * @return {boolean}
  */
-
 const isBalanced = (root) => {
   if (!root) return true
 
-  let leftHeight = getHeight(root.left)
-  let rightHeight = getHeight(root.right)
+  const leftHeight = getHeight(root.left)
+  const rightHeight = getHeight(root.right)
 
   return (
     Math.abs(leftHeight - rightHeight) <= 1 &&
     isBalanced(root.left) &&
     isBalanced(root.right)
-  );
-};
-
-const getHeight = (root) => {
-  if (!root) return 0
-
-  return 1 + Math.max(getHeight(root.left), getHeight(root.right))
+  )
 }
