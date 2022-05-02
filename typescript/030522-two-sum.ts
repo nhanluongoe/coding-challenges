1158125fae5e7ec75749e455700078942f83cf0f
nhanluongoe <nhanlt18@gmail.com>
2022-05-03 01:10:37 +0700

    fix(typescript): fix variable used before assignment

--- typescript/030522-two-sum.ts
+++ typescript/030522-two-sum.ts
@@ -1,12 +1,14 @@
 function twoSum(nums: number[], target: number): number[] {
   const mappedNums = new Map(nums.map((num, idx) => [num, idx]));
-  let res: number[];
+  let res: number[] = [];
 
   nums.forEach((num, idx) => {
     const remain = target - num;
-    const remainIdx = mappedNums.get(remain)
-    if (remainIdx !== undefined && remainIdx !== idx)
-      res = [idx, mappedNums.get(remain)];
+    const remainIdx = mappedNums.get(remain);
+    if (remainIdx !== undefined && remainIdx !== idx) {
+      res.push(idx);
+      res.push(remainIdx);
+    }
   });
 
   return res;
