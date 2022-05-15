c2869cc6f60b4f71319bb136cbb07956485c5fca
nhanluongoe <nhanlt18@gmail.com>
2022-05-15 16:35:44 +0700

    challenge(javascript): add second solution (slower)

--- javascript/15052022-merge-sorted-array.js
+++ javascript/15052022-merge-sorted-array.js
@@ -24,32 +24,52 @@ function mergeArrays(myArray, alicesArray) {
   return result
 }
 
+/**
+ * Time complexity: O(logn)
+ * Space complexity: O(n)
+ */
+function mergeArrays2(myArray, alicesArray) {
+  const result = myArray.concat(alicesArray)
+  result.sort((a, b) => a - b)
+  return result
+}
+
 // Tests
 
 let desc = 'both arrays are empty'
 let actual = mergeArrays([], [])
+let actual2 = mergeArrays2([], [])
 let expected = []
 assertDeepEqual(actual, expected, desc)
+assertDeepEqual(actual2, expected, desc)
 
 desc = 'first array is empty'
 actual = mergeArrays([], [1, 2, 3])
+actual2 = mergeArrays2([], [1, 2, 3])
 expected = [1, 2, 3]
 assertDeepEqual(actual, expected, desc)
+assertDeepEqual(actual2, expected, desc)
 
 desc = 'second array is empty'
 actual = mergeArrays([5, 6, 7], [])
+actual2 = mergeArrays([5, 6, 7], [])
 expected = [5, 6, 7]
 assertDeepEqual(actual, expected, desc)
+assertDeepEqual(actual2, expected, desc)
 
 desc = 'both arrays have some numbers'
 actual = mergeArrays([2, 4, 6], [1, 3, 7])
+actual2 = mergeArrays([2, 4, 6], [1, 3, 7])
 expected = [1, 2, 3, 4, 6, 7]
 assertDeepEqual(actual, expected, desc)
+assertDeepEqual(actual2, expected, desc)
 
 desc = 'arrays are different lengths'
 actual = mergeArrays([2, 4, 6, 8], [1, 7])
+actual2 = mergeArrays([2, 4, 6, 8], [1, 7])
 expected = [1, 2, 4, 6, 7, 8]
 assertDeepEqual(actual, expected, desc)
+assertDeepEqual(actual2, expected, desc)
 
 function assertDeepEqual(a, b, testDesc) {
   const aStr = JSON.stringify(a)
