c4dac0f9fe7cd836949ed8262528b35ea14e8882
nhanluongoe <nhanlt18@gmail.com>
2022-05-25 22:42:50 +0700

    challenge(go): add O(1) space solution

--- go/250522-find-duplication.go
+++ go/250522-find-duplication.go
@@ -13,15 +13,50 @@ import (
  */
 
 func main() {
-	slice1 := []int{4, 1, 4, 8, 3, 2, 7, 6, 5}
-	result1 := findRepeat(slice1)
+	slice := []int{4, 1, 4, 8, 3, 2, 7, 6, 5}
+
+	result1 := findRepeat(slice)
 	fmt.Println(result1)
 
+	result2 := findRepeat2(slice)
+	fmt.Println(result2)
+}
+
+func findRepeat(numbers []int) int {
+	floor := 1
+	ceiling := len(numbers) - 1
+
+	for floor < ceiling {
+		midPoint := floor + (ceiling-floor)/2
+		lowerRangeFloor := floor
+		lowerRangeCeiling := midPoint
+		upperRangeFloor := midPoint + 1
+		upperRangeCeiling := ceiling
+
+		distinctPosiibleIntegersInLowerRange := lowerRangeCeiling - lowerRangeFloor + 1
+
+		integersInLowerRange := 0
+		for i := 0; i < len(numbers); i++ {
+			if numbers[i] >= lowerRangeFloor && numbers[i] <= lowerRangeCeiling {
+				integersInLowerRange += 1
+			}
+		}
+
+		if integersInLowerRange > distinctPosiibleIntegersInLowerRange {
+			floor = lowerRangeFloor
+			ceiling = lowerRangeCeiling
+		} else {
+			floor = upperRangeFloor
+			ceiling = upperRangeCeiling
+		}
+	}
+
+	return floor
 }
 
 // Time complexity: O(nlogn)
 // Space complexity: O(n)
-func findRepeat(slice []int) int {
+func findRepeat2(slice []int) int {
 	coppiedSlice := make([]int, len(slice))
 	copy(coppiedSlice, slice)
 	sort.Ints(coppiedSlice)
