f86c44140170ff359a1482dcf129504ab820b4e2
nhanluongoe <nhanlt18@gmail.com>
2022-06-06 22:05:55 +0700

    challenge(java): add better solution for max-stack

--- java/LargestStack.java
+++ java/LargestStack.java
@@ -54,3 +54,31 @@ public class LargestStack {
     System.out.println(s.getMax());
   }
 }
+
+/**
+ * Time Complexity: O(1)
+ * Space complexity: O(n)
+ */
+class MaxStack {
+  private Deque<Integer> stack = new ArrayDeque<>();
+  private Deque<Integer> maxesStack = new ArrayDeque<>();
+
+  public void push(int item) {
+    stack.push(item);
+    if (maxesStack.isEmpty() || item >= maxesStack.peek()) {
+      maxesStack.push(item);
+    }
+  }
+
+  public int pop() {
+    int item = stack.pop();
+    if (item == maxesStack.peek()) {
+      maxesStack.pop();
+    }
+    return item;
+  }
+
+  public int getMax() {
+    return maxesStack.peek();
+  }
+}
