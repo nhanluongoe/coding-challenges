04e548e77803773ddf0232b4e1d280e69620c6cf
nhanluongoe <nhanlt18@gmail.com>
2022-06-08 22:35:14 +0700

    challenge(java): add set matrix zero

--- java/SetMatrixZero.java
+++ java/SetMatrixZero.java
@@ -2,7 +2,7 @@ import java.util.*;
 
 public class SetMatrixZero {
 
-  public void setZeroes(int[][] matrix) {
+  public static void setZeroes(int[][] matrix) {
     List<Integer> zeroX = new ArrayList<>();
     List<Integer> zeroY = new ArrayList<>();
 
@@ -18,15 +18,26 @@ public class SetMatrixZero {
 
     for (int i = 0; i < matrix.length; i++) {
       for (int j = 0; j < matrix[0].length; j++) {
-        if (zeroX.contains(i)) {
-          matrix[i][j] = 0;
-        }
-        if (zeroY.contains(j)) {
+        if (zeroX.contains(i) || zeroY.contains(j)) {
           matrix[i][j] = 0;
         }
       }
     }
   }
 
+  public static void print(int[][] matrix) {
+    for (int i = 0; i < matrix.length; i++) {
+      for (int j = 0; j < matrix[0].length; j++) {
+        System.out.print(matrix[i][j] + " ");
+      }
+      System.out.println();
+    }
+  }
 
+  public static void main(String[] args) {
+    int[][] matrix = { { 0, 1, 2, 0 }, { 3, 4, 5, 2 }, { 1, 3, 1, 5 } };
+    setZeroes(matrix);
+
+    print(matrix);
+  }
 }
