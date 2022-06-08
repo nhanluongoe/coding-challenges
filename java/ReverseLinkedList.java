b6932d94b2cd0a5336c63279b74b4cb4456b738f
nhanluongoe <nhanlt18@gmail.com>
2022-06-08 23:21:47 +0700

    challenge(java): add reverse linked list

--- java/ReverseLinkedList.java
+++ java/ReverseLinkedList.java
@@ -8,7 +8,7 @@ public class ReverseLinkedList {
     }
   }
 
-  public static LinkedListNode append (LinkedListNode head, int value) {
+  public static LinkedListNode append(LinkedListNode head, int value) {
     LinkedListNode current = head;
     while (current.next != null) {
       current = current.next;
@@ -17,19 +17,33 @@ public class ReverseLinkedList {
     return current.next;
   }
 
+  public static LinkedListNode reverse(LinkedListNode head) {
+    if (head == null) return head;
+
+    LinkedListNode previous = null;
+    LinkedListNode current = head;
+
+    while (current.next != null) {
+      LinkedListNode next = current.next;
+      current.next = previous;
+      previous = current;
+      current = next;
+    }
+    current.next = previous;
+
+    return current;
+  }
 
   public static void main(String[] args) {
     LinkedListNode head = new LinkedListNode(1);
     append(head, 2);
     append(head, 3);
-    LinkedListNode tail = append(head, 4);
-    tail.next = head;
+    append(head, 4);
 
-    LinkedListNode head2 = new LinkedListNode(1);
-    append(head2, 2);
-    append(head2, 3);
-    append(head2, 4);
+    LinkedListNode newHead = reverse(head);
+    System.out.println(newHead.value); // expect 4
+    System.out.println(newHead.next.value); // expect 3
 
-    LinkedListNode head3 = new LinkedListNode(1);
+
   }
 }
