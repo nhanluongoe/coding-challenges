720a83acc6d1be4bfe46b4c8a948ab71a540376a
nhanluongoe <nhanlt18@gmail.com>
2022-06-22 23:28:38 +0700

    refactor(java): use better variable name and more sense logic

--- java/sliding-window/LongestSubstringKDistinct.java
+++ java/sliding-window/LongestSubstringKDistinct.java
@@ -1,25 +1,15 @@
 import java.util.*;
 
 public class LongestSubstringKDistinct {
-  
+
   public static int findLongestSubstringKDistinct(int K, String string) {
     int windowStartIndex = 0;
     int maxLength = Integer.MIN_VALUE;
     Map<Character, Integer> charMap = new HashMap<>();
 
     for (int windowEndIndex = 0; windowEndIndex < string.length(); windowEndIndex++) {
-      char currentChar = string.charAt(windowEndIndex);
-
-      if (charMap.containsKey(currentChar)) {
-        charMap.put(currentChar, charMap.get(currentChar) + 1);
-      } else {
-        charMap.put(currentChar, 1);
-      }
-
-
-      if (charMap.size() <= K) {
-        maxLength = Math.max(maxLength, windowEndIndex - windowStartIndex + 1);
-      }
+      char windowEndChar = string.charAt(windowEndIndex);
+      charMap.put(windowEndChar, charMap.getOrDefault(windowEndChar, 0) + 1);
 
       while (charMap.size() > K) {
         char windowStartChar = string.charAt(windowStartIndex);
@@ -29,6 +19,8 @@ public class LongestSubstringKDistinct {
         }
         windowStartIndex += 1;
       }
+
+      maxLength = Math.max(maxLength, windowEndIndex - windowStartIndex + 1);
     }
 
     return maxLength;
