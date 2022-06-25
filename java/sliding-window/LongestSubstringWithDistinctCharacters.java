1be182edd5e5c1da8124939a21b920075161f52d
nhanluongoe <nhanlt18@gmail.com>
2022-06-26 00:32:10 +0700

    challenge(java): solve longest substring with distinct characters with better solution

--- java/sliding-window/LongestSubstringWithDistinctCharacters.java
+++ java/sliding-window/LongestSubstringWithDistinctCharacters.java
@@ -1,6 +1,24 @@
 import java.util.*;
 
 public class LongestSubstringWithDistinctCharacters {
+  public static int findLengthBetter(String string) {
+    int windowStart = 0, maxLength = 0;
+
+    Map<Character, Integer> charIndexMap = new HashMap<>();
+    for (int windowEnd = 0; windowEnd < string.length(); windowEnd++) {
+      char rightChar = string.charAt(windowEnd);
+
+      if (charIndexMap.containsKey(rightChar)) {
+        windowStart = Math.max(windowStart, charIndexMap.get(rightChar) + 1);
+      }
+      charIndexMap.put(rightChar, windowEnd);
+      
+      maxLength = Math.max(maxLength, windowEnd - windowStart + 1);
+    }
+
+    return maxLength;
+  }
+
   public static int findLength(String string) {
     int startIndex = 0;
     int maxLength = 0;
@@ -27,5 +45,7 @@ public class LongestSubstringWithDistinctCharacters {
         + findLength("abbbb")); // expects 2 "ab"
     System.out.println("Length of the longest substring: "
         + findLength("abccde")); // expects 3 "abc" or "cde"
+        System.out.println("Length of the longest substring: "
+        + findLengthBetter("aabccabdefgh")); // expects 8 "cabdefgh"
   }
 }
