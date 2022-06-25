365ccf3f7521c46e50ed09e4b64a0ba093a2d477
nhanluongoe <nhanlt18@gmail.com>
2022-06-25 23:45:21 +0700

    challenge(java): solve longest substring with distinct characters

--- java/sliding-window/LongestSubstringWithDistinctCharacters.java
+++ java/sliding-window/LongestSubstringWithDistinctCharacters.java
@@ -1,15 +1,31 @@
+import java.util.*;
+
 public class LongestSubstringWithDistinctCharacters {
   public static int findLength(String string) {
+    int startIndex = 0;
+    int maxLength = 0;
 
-    return 0;
+    Set<Character> characterSet = new HashSet<>();
+    for (int endIndex = 0; endIndex < string.length(); endIndex++) {
+      char endCharacter = string.charAt(endIndex);
+      if (characterSet.contains(endCharacter)) {
+        characterSet.clear();
+        startIndex = endIndex;
+      } else {
+        characterSet.add(endCharacter);
+        maxLength = Math.max(endIndex - startIndex + 1, maxLength);
+      }
+    }
+
+    return maxLength;
   }
 
   public static void main(String[] args) {
     System.out.println("Length of the longest substring: "
-        + findLength("aabccbb"));
+        + findLength("aabccbb")); // expects 3 "abc"
     System.out.println("Length of the longest substring: "
-        + findLength("abbbb"));
+        + findLength("abbbb")); // expects 2 "ab"
     System.out.println("Length of the longest substring: "
-        + findLength("abccde"));
+        + findLength("abccde")); // expects 3 "abc" or "cde"
   }
 }
