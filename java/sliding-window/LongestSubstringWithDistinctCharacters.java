bc5a455831d790b3e27e714e122a221fd8aba06c
nhanluongoe <nhanlt18@gmail.com>
2022-06-25 23:20:03 +0700

    challenge(java): init longest substring with distinct characters

---
+++ java/sliding-window/LongestSubstringWithDistinctCharacters.java
@@ -0,0 +1,15 @@
+public class LongestSubstringWithDistinctCharacters {
+  public static int findLength(String string) {
+
+    return 0;
+  }
+
+  public static void main(String[] args) {
+    System.out.println("Length of the longest substring: "
+        + findLength("aabccbb"));
+    System.out.println("Length of the longest substring: "
+        + findLength("abbbb"));
+    System.out.println("Length of the longest substring: "
+        + findLength("abccde"));
+  }
+}
