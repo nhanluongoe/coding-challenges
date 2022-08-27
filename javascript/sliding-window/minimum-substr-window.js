4fac56963e0a4b79308deec8fc40fecc29c0dd53
nhanluongoe <nhanlt18@gmail.com>
2022-08-27 21:49:30 +0700

    javascript(challenge): init smallest window containing substring problem

---
+++ javascript/sliding-window/minimum-substr-window.js
@@ -0,0 +1,7 @@
+function findSubString(string, pattern) { 
+}
+
+console.log(findSubString('aabdec', 'abc'));
+console.log(findSubString('aabdec', 'abac'));
+console.log(findSubString('abdbca', 'abc'));
+console.log(findSubString('adcad', 'abc'));
