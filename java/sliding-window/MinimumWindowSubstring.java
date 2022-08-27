e4f897a845c73b4464f6ad7fa3adc6ffe4ecfc83
nhanluongoe <nhanlt18@gmail.com>
2022-08-27 14:49:05 +0700

    java(challenge): init smallest window substring problem

---
+++ java/sliding-window/MinimumWindowSubstring.java
@@ -0,0 +1,13 @@
+class MinimumWindowSubstring {
+  public static String findSubstring(String str, String pattern) {
+
+    return "str";
+  }
+
+  public static void main(String[] args) {
+    System.out.println(MinimumWindowSubstring.findSubstring("aabdec", "abc"));
+    System.out.println(MinimumWindowSubstring.findSubstring("aabdec", "abac"));
+    System.out.println(MinimumWindowSubstring.findSubstring("abdbca", "abc"));
+    System.out.println(MinimumWindowSubstring.findSubstring("adcad", "abc"));
+}
+}
