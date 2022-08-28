32e18a06e6391a9b20134335be9fb7e89439dc1c
nhanluongoe <nhanlt18@gmail.com>
2022-08-28 12:18:09 +0700

    javascript(challenge): solve word concatenation problem

---
+++ javascript/sliding-window/word-concatenation.js
@@ -0,0 +1,29 @@
+function findWordConcatenation(str, words) {
+  const wordFrequencyMap = new Map()
+  words.forEach((word) =>
+    wordFrequencyMap.set(word, (wordFrequencyMap.get(word) ?? 0) + 1)
+  )
+
+  const resultIndices = []
+  const wordsCount = words.length
+  const wordLength = words[0].length
+  for (let i = 0; i <= str.length - wordsCount * wordLength; i++) {
+    const wordsSeen = new Map()
+    for (let j = 0; j < wordsCount; j++) {
+      const nextWordIndex = i + j * wordLength
+      const word = str.substring(nextWordIndex, nextWordIndex + wordLength)
+      if (!wordFrequencyMap.has(word)) break
+
+      wordsSeen.set(word, (wordsSeen.get(word) ?? 0) + 1)
+
+      if (wordsSeen.get(word) > wordFrequencyMap.get(word)) break
+
+      if (j + 1 === wordsCount) resultIndices.push(i)
+    }
+  }
+
+  return resultIndices
+}
+
+console.log(findWordConcatenation('catfoxcat', ['cat', 'fox']))
+console.log(findWordConcatenation('catcatfoxfox', ['cat', 'fox']))
