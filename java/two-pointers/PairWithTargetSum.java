b759a795de41068f8433f5d24205905eec6dcea5
nhanluongoe <nhanlt18@gmail.com>
2022-08-29 16:48:58 +0700

    java(challenge): solve pair with target sum problem

--- java/two-pointers/PairWithTargetSum.java
+++ java/two-pointers/PairWithTargetSum.java
@@ -1,11 +1,28 @@
-public class PairWithTargetSum {
-  
-  public static int[] search(int[] arr, int targetSum) {
+import java.util.Arrays;
 
+public class PairWithTargetSum {
+
+  public static int[] search(int[] arr, int targetSum) {
+    int startIndex = 0, endIndex = arr.length - 1;
+
+    while (arr[startIndex] + arr[endIndex] != targetSum) {
+      if (arr[startIndex] + arr[endIndex] > targetSum) {
+        endIndex--;
+        continue;
+      }
+
+      if (arr[startIndex] + arr[endIndex] < targetSum) {
+        startIndex++;
+        continue;
+      }
+    }
+
+    return new int[] { startIndex, endIndex };
   }
 
   public static void main(String[] args) {
-    System.out.println(PairWithTargetSum.search(new int[]{1, 2, 3, 4, 5, 6}, 6));
-    System.out.println(PairWithTargetSum.search(new int[]{2, 5, 9, 11}, 11));
+    System.out.println(new int[]{1, 2, 3});
+    System.out.println(Arrays.toString(PairWithTargetSum.search(new int[] { 1, 2, 3, 4, 6 }, 6)));
+    System.out.println(Arrays.toString(PairWithTargetSum.search(new int[] { 2, 5, 9, 11 }, 11)));
   }
 }
