c975ffbc48c3e5511f23c9e6b2648267eb943ad5
nhanluongoe <nhanlt18@gmail.com>
2022-08-29 17:29:39 +0700

    java(challenge): solve remove duplicates problem

--- java/two-pointers/RemoveDuplicates.java
+++ java/two-pointers/RemoveDuplicates.java
@@ -3,11 +3,20 @@
  */
 public class RemoveDuplicates {
   public static int remove(int[] arr) {
+    int nextNonDuplicate = 1;
 
-    return -1;
+    for (int i = 1; i < arr.length; i++) {
+      if (arr[i - 1] != arr[i]) {
+        arr[nextNonDuplicate] = arr[i];
+        nextNonDuplicate++;
+      }
+    }
+
+    return nextNonDuplicate;
   }
 
   public static void main(String[] args) {
-    
+    System.out.println(remove(new int[]{2, 3, 3, 3, 6, 9, 9}));
+    System.out.println(remove(new int[]{2, 2, 2, 11})); 
   }
 }
