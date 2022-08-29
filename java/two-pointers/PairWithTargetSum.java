a43c8d0d387b13b82ecc32c5bd63d639d68e261f
nhanluongoe <nhanlt18@gmail.com>
2022-08-29 16:55:44 +0700

    java(refactor): hopefully to make it more readable

--- java/two-pointers/PairWithTargetSum.java
+++ java/two-pointers/PairWithTargetSum.java
@@ -12,23 +12,21 @@ public class PairWithTargetSum {
   public static int[] search(int[] arr, int targetSum) {
     int startIndex = 0, endIndex = arr.length - 1;
 
-    while (arr[startIndex] + arr[endIndex] != targetSum) {
-      if (arr[startIndex] + arr[endIndex] > targetSum) {
-        endIndex--;
-        continue;
-      }
+    while (startIndex < endIndex) {
+      int currentSum = arr[startIndex] + arr[endIndex];
+      if (currentSum == targetSum)
+        return new int[] { startIndex, endIndex };
 
-      if (arr[startIndex] + arr[endIndex] < targetSum) {
+      if (currentSum > targetSum)
+        endIndex--;
+      else
         startIndex++;
-        continue;
-      }
     }
 
-    return new int[] { startIndex, endIndex };
+    return new int[] { -1, -1 }; // not found
   }
 
   public static void main(String[] args) {
-    System.out.println(new int[]{1, 2, 3});
     System.out.println(Arrays.toString(PairWithTargetSum.search(new int[] { 1, 2, 3, 4, 6 }, 6)));
     System.out.println(Arrays.toString(PairWithTargetSum.search(new int[] { 2, 5, 9, 11 }, 11)));
   }
