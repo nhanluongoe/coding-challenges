e0023c5cc8f990ff8aac2f14fee133da05803d72
nhanluongoe <nhanlt18@gmail.com>
2022-08-29 16:39:54 +0700

    java(challenge): init pair with target sum problem

---
+++ java/two-pointers/PairWithTargetSum.java
@@ -0,0 +1,11 @@
+public class PairWithTargetSum {
+  
+  public static int[] search(int[] arr, int targetSum) {
+
+  }
+
+  public static void main(String[] args) {
+    System.out.println(PairWithTargetSum.search(new int[]{1, 2, 3, 4, 5, 6}, 6));
+    System.out.println(PairWithTargetSum.search(new int[]{2, 5, 9, 11}, 11));
+  }
+}
