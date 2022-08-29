2775efdba3657d0477fc2e51f58f72a126e4b8f7
nhanluongoe <nhanlt18@gmail.com>
2022-08-29 20:50:15 +0700

    challenge(java): solve sorted array squares problem

--- java/two-pointers/SortedArraySquares.java
+++ java/two-pointers/SortedArraySquares.java
@@ -1,9 +1,31 @@
+import java.util.Arrays;
+
+/**
+ * Constraint: input array is sorted
+ */
 public class SortedArraySquares {
   public static int[] makeSquares(int[] arr) {
+    int[] resultArray = new int[arr.length];
+    int higherSquareIndex = arr.length - 1;
+    int left = 0, right = arr.length - 1;
+    while (left <= right) {
+      int leftSquare = arr[left] * arr[left];
+      int rightSquare = arr[right] * arr[right];
 
-    return new int[]{1, 1};
+      if (leftSquare >= rightSquare) {
+        resultArray[higherSquareIndex--] = leftSquare;
+        left++;
+      } else {
+        resultArray[higherSquareIndex--] = rightSquare;
+        right--;
+      }
+    }
+
+    return resultArray;
   }
-  public static void main(String[] args) {
 
-  }  
+  public static void main(String[] args) {
+    System.out.println(Arrays.toString(makeSquares(new int[] { -2, -1, 0, 2, 3 })));
+    System.out.println(Arrays.toString(makeSquares(new int[] { -3, -1, 0, 1, 2 })));
+  }
 }
