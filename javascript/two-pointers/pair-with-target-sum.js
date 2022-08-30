b9f73fd5e42431c86a376da21daa305d1655b198
nhanluongoe <nhanlt18@gmail.com>
2022-08-30 13:25:26 +0700

    challenge(javascript): solve pair with targetsum problem

--- javascript/two-pointers/pair-with-target-sum.js
+++ javascript/two-pointers/pair-with-target-sum.js
@@ -1,9 +1,22 @@
-/** 
+/**
  * Constraint: input array is sorted
+ * Time complexity: O(n)
+ * Space complexity: O(1)
  */
 function search(arr, target) {
+  let left = 0
+  let right = arr.length - 1
 
+  while (left < right) {
+    const currentSum = arr[left] + arr[right]
+    if (currentSum === target) return [left, right]
+
+    if (currentSum > target) right--
+    else left++
+  }
+
+  return [-1, -1] // not found
 }
 
-console.log(search([ 2, 5, 9, 11 ], 6));
-console.log(search([1, 2, 3, 4, 6 ], 6));
+console.log(search([2, 5, 9, 11], 11))
+console.log(search([1, 2, 3, 4, 6], 6))
