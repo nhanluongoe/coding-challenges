457d43e313885d1700b4e4b29b3673d43ce91187
nhanluongoe <nhanlt18@gmail.com>
2022-08-30 14:08:44 +0700

    challenge(java): init trimplet sum to zero problem

---
+++ java/two-pointers/TripletSumToZero.java
@@ -0,0 +1,10 @@
+import java.util.*;
+
+public class TripletSumToZero {
+  public static List<List<Integer>> searchTriplets(int[] arr) {
+    
+  }
+  public static void main(String[] args) {
+
+  }
+}
