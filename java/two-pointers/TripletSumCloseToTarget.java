095e3217334b117f26eaa91f4ecf64a7331facff
nhanluongoe <nhanlt18@gmail.com>
2022-08-30 15:47:21 +0700

    challenge(java): init triplet sum close to target problem

---
+++ java/two-pointers/TripletSumCloseToTarget.java
@@ -0,0 +1,14 @@
+/**
+ * Constraint: If there're more than one such triplets, return the triplet with smallest sum
+ */
+public class TripletSumCloseToTarget {
+  public static int searchTriplet(int[] arr, int targetSum) {
+
+    return -1;
+  }
+  
+  public static void main() {
+    System.out.println(searchTriplet(new int[]{-3, -1, 1, 2}, 1));
+    System.out.println(searchTriplet(new int[]{1, 0, 1, 1}, 100));
+  }
+}
