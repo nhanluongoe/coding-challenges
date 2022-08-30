da050ef774081b0837d44c216531e52cf04ceb6a
nhanluongoe <nhanlt18@gmail.com>
2022-08-30 15:51:44 +0700

    chore(java): add expected value

--- java/two-pointers/TripletSumCloseToTarget.java
+++ java/two-pointers/TripletSumCloseToTarget.java
@@ -8,7 +8,8 @@ public class TripletSumCloseToTarget {
   }
   
   public static void main() {
-    System.out.println(searchTriplet(new int[]{-3, -1, 1, 2}, 1));
-    System.out.println(searchTriplet(new int[]{1, 0, 1, 1}, 100));
+    System.out.println(searchTriplet(new int[]{-2, 0, 1, 2}, 2)); // -2, 1, 2 => 1
+    System.out.println(searchTriplet(new int[]{-3, -1, 1, 2}, 1)); // -3, 1, 2 => 0
+    System.out.println(searchTriplet(new int[]{1, 0, 1, 1}, 100)); // 1, 1, 1 => 3
   }
 }
