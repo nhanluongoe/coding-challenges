b1bf0c81911fb604ef2b797ba0a6378056d64cc3
nhanluongoe <nhanlt18@gmail.com>
2022-08-30 13:39:25 +0700

    challenge(javascript): solve remove duplicates problem

--- javascript/two-pointers/remove-duplicates.js
+++ javascript/two-pointers/remove-duplicates.js
@@ -1,8 +1,18 @@
 /**
  * Constraint: input array is sorted
+ * Time complexity: O(n)
+ * Space complexity: O(1)
  */
 function remove(arr) {
+  let nextNonDuplicate = 1;
 
+  for (let i = 1; i < arr.length; i++) {
+    if (arr[i - 1] !== arr[i]) {
+      arr[nextNonDuplicate++] = arr[i];
+    }
+  }
+
+  return nextNonDuplicate;
 }
 
 console.log(remove([2, 3, 3, 3, 6, 9, 9 ]))
