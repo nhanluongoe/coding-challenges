2bebf30b2a8d6b564e60fbdc916b6e0a856656a7
nhanluongoe <nhanlt18@gmail.com>
2022-08-30 13:43:15 +0700

    challenge(javascript): init sorted array squares problem"

---
+++ javascript/two-pointers/sorted-array-squares.js
@@ -0,0 +1,9 @@
+/**
+ * Constraint: input array is sorted
+ */
+function sortArraySquares(arr) {
+
+}
+
+console.log(sortArraySquares([-2, -1, 0, 2, 3]))
+console.log(sortArraySquares([-3, -1, 0, 1, 2 ]))
