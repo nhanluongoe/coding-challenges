d89d3fa4fcb6c68373adf340ee392ee38cb66351
nhanluongoe <nhanlt18@gmail.com>
2022-08-30 13:47:45 +0700

    challenge(javascript): solve sorted array squares problem"

--- javascript/two-pointers/sorted-array-squares.js
+++ javascript/two-pointers/sorted-array-squares.js
@@ -2,7 +2,25 @@
  * Constraint: input array is sorted
  */
 function sortArraySquares(arr) {
+  let left = 0;
+  let right = arr.length - 1;
+  let higherSquareIndex = arr.length - 1;
+  const resultArray = new Array(arr.length);
 
+  while (left <= right) {
+    const leftSquare = arr[left] * arr[left]
+    const rightSquare = arr[right] * arr[right]
+
+    if (leftSquare >= rightSquare) {
+      resultArray[higherSquareIndex--] = leftSquare;
+      left++;
+    } else {
+      resultArray[higherSquareIndex--] = rightSquare;
+      right--;
+    }
+  }
+
+  return resultArray;
 }
 
 console.log(sortArraySquares([-2, -1, 0, 2, 3]))
