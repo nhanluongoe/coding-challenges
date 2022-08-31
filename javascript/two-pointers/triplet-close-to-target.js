a100a65ce93162e30e50029e42c80811f4d0194e
nhanluongoe <nhanlt18@gmail.com>
2022-08-31 11:32:11 +0700

    challenge(javascript): init triplet-sum-close-to-target problem

---
+++ javascript/two-pointers/triplet-close-to-target.js
@@ -0,0 +1,10 @@
+/**
+ * Constraint: If there're more than one such triplets, return the triplet with smallest sum
+ */
+function searchTriplet() {
+
+}
+
+console.log(searchTriplet([-2, 0, 1, 2], 2)) // -2, 1, 2 => 1
+console.log(searchTriplet([-3, -1, 1, 2], 1)) // -3, 1, 2 => 0
+console.log(searchTriplet([1, 0, 1, 1], 100)) // 1, 1, 1 => 3
