d263bcb4921ab167f77bd1e9797003b0f00dd533
nhanluongoe <nhanlt18@gmail.com>
2022-08-31 21:40:48 +0700

    challenge(java): solve triplet-smaller-sum problem

--- java/two-pointers/TripletWithSmallerSum.java
+++ java/two-pointers/TripletWithSmallerSum.java
@@ -1,9 +1,29 @@
+import java.util.Arrays;
+
 public class TripletWithSmallerSum {
   public static int searchTriplets(int[] arr, int target) {
+    Arrays.sort(arr);
 
-    return -1;
+    int result = 0;
+    for (int i = 0; i < arr.length - 2; i++) {
+      int left = i + 1, right = arr.length - 1;
+
+      while (left < right) {
+        int currentSum = arr[i] + arr[left] + arr[right];
+
+        if (currentSum < target) {
+          result += right - left;
+          left++;
+        } else {
+          right--;
+        }
+      }
+    }
+
+    return result;
   }  
   public static void main(String[] args) {
-
+    System.out.println(searchTriplets(new int[]{-1, 0, 2, 3}, 3)); // [-1, 0, 3], [-1, 0, 2]
+    System.out.println(searchTriplets(new int[]{-1, 4, 2, 1, 3}, 5)); // [-1, 1, 4], [-1, 1, 3], [-1, 1, 2], [-1, 2, 3]
   }
 }
