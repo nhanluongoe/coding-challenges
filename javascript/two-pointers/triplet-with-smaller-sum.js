c29617e3c4398e2270667c2a5a9afb3d4264252b
nhanluongoe <nhanlt18@gmail.com>
2022-09-01 16:26:16 +0700

    challenge(javascript): solve triplet-smaller-sum problem"

--- javascript/two-pointers/triplet-with-smaller-sum.js
+++ javascript/two-pointers/triplet-with-smaller-sum.js
@@ -1,5 +1,22 @@
 function searchTriplets(arr, target) {
+  let result = 0;  
 
+  arr.sort();
+  for (let i = 0; i < arr.length-2; i++) {
+    let left = i + 1; 
+    let right = arr.length - 1;
+
+    while (left < right) {
+      const currentSum = arr[i] + arr[left] + arr[right];
+
+      if (currentSum < target) {
+        result += right - left;
+        left++;
+      } else right--;
+    }
+  }
+
+  return result;
 }
 
 console.log(searchTriplets([-1, 0, 2, 3], 3)) // [-1, 0, 3], [-1, 0, 2]
