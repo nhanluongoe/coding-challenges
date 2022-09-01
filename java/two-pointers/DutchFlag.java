6821514d6a3d749f12ca1f36856550e7975448e5
nhanluongoe <nhanlt18@gmail.com>
2022-09-01 17:24:12 +0700

    challenge(java): solve dutch-flag problem

--- java/two-pointers/DutchFlag.java
+++ java/two-pointers/DutchFlag.java
@@ -2,7 +2,28 @@ import java.util.*;
 
 public class DutchFlag {
   public static void sort(int[] arr) {
+    int low = 0, high = arr.length - 1;
 
+    for (int i = 0; i <= high;) {
+      int currentNum = arr[i];
+      if (currentNum == 0) {
+        swap(arr, i, low);
+        low++;
+        i++;
+      } else if (currentNum == 1) {
+        i++;
+      } else {
+        swap(arr, i, high);
+        high--;
+      }
+    }
+
+  }
+  
+  public static void swap(int[] arr, int i, int j) {
+    int temp = arr[i];
+    arr[i] = arr[j];
+    arr[j] = temp;
   }
   
   public static void main(String[] args) {
