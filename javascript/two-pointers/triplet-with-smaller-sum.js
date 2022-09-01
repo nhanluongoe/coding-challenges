c96dbca166dce195b2e6792a67c7d9b769417a3e
nhanluongoe <nhanlt18@gmail.com>
2022-09-01 16:21:13 +0700

    challenge(javascript): init triplet-smaller-sum problem

---
+++ javascript/two-pointers/triplet-with-smaller-sum.js
@@ -0,0 +1,6 @@
+function searchTriplets(arr, target) {
+
+}
+
+console.log(searchTriplets([-1, 0, 2, 3], 3)) // [-1, 0, 3], [-1, 0, 2]
+console.log(searchTriplets([-1, 4, 2, 1, 3], 5)) // [-1, 1, 4], [-1, 1, 3], [-1, 1, 2], [-1, 2, 3]
