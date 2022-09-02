ba16c20484af1b20b50271d741aa62bd876f0ade
nhanluongoe <nhanlt18@gmail.com>
2022-09-02 15:04:03 +0700

    challenge(java): init happy-number problem

---
+++ java/fast-slow-pointers/HappyNumber.java
@@ -0,0 +1,10 @@
+public class HappyNumber {
+  public static boolean find(int number) {
+
+    return false;
+  }
+  public static void main(String[] args) {
+    System.out.println(find(23));
+    System.out.println(find(12));
+  }  
+}
