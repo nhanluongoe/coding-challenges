633f8cdedfb11c6b07d79974b5623215328d5095
nhanluongoe <nhanlt18@gmail.com>
2022-09-02 08:20:07 +0700

    challenge(javascript): solve dutch-flag problem

--- javascript/two-pointers/dutch-flag.js
+++ javascript/two-pointers/dutch-flag.js
@@ -1,5 +1,27 @@
 function dutchFlagSort(arr) {
+  let low = 0; 
+  let high = arr.length - 1;
 
+  for (let i = 0; i <= high;) {
+    const currentNumber = arr[i];
+    if (currentNumber === 0) {
+      swap(arr, low, i);
+      low++;
+      i++;
+    } else if (currentNumber === 1) {
+      i++;
+    } else {
+      swap(arr, high, i);
+      high--;
+    }
+  }
+
+}
+
+function swap(arr, i, j) {
+  const temp = arr[i];
+  arr[i] = arr[j];
+  arr[j] = temp;
 }
 
 const arr1 = [1, 0, 2, 1, 0];
