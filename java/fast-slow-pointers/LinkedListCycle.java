151a7517331eefa935b51b7078f3c109983c653d
nhanluongoe <nhanlt18@gmail.com>
2022-09-02 13:20:13 +0700

    challenge(java): init linked-list-cycle problem

---
+++ java/fast-slow-pointers/LinkedListCycle.java
@@ -0,0 +1,31 @@
+class ListNode {
+  int value = 0;
+  ListNode next;
+  public ListNode(int value) {
+    this.value = value; 
+  }
+}
+public class LinkedListCycle {
+  public static boolean hasCycle(ListNode node) {
+
+    return false;
+  }  
+
+  public static void main(String[] args) {
+    ListNode head = new ListNode(1);
+    head.next = new ListNode(2);
+    head.next.next = new ListNode(3);
+    head.next.next.next = new ListNode(4);
+    head.next.next.next.next = new ListNode(5);
+    head.next.next.next.next.next = new ListNode(6);
+
+    System.out.println(hasCycle(head));
+
+    head.next.next.next.next.next.next = head.next.next;
+    System.out.println(hasCycle(head));
+
+
+    head.next.next.next.next.next.next = head.next.next.next;
+    System.out.println(hasCycle(head));
+  }
+}
