65674cfa85a600dd7595e9c60e0aeaee2bbc24e0
nhanluongoe <nhanlt18@gmail.com>
2022-09-02 08:09:14 +0700

    challenge(javascript): init dutch-flag problem

---
+++ javascript/two-pointers/dutch-flag.js
@@ -0,0 +1,11 @@
+function dutchFlagSort(arr) {
+
+}
+
+const arr1 = [1, 0, 2, 1, 0];
+dutchFlagSort(arr1);
+console.log(arr1);
+
+const arr2 = [2, 2, 0, 1, 2, 0];
+dutchFlagSort(arr2);
+console.log(arr2);
