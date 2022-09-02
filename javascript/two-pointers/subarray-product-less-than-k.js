91d1d75883f8ee86feadeb6e89334880083e8200
nhanluongoe <nhanlt18@gmail.com>
2022-09-02 08:00:13 +0700

    challenge(javascript): solve subarray-product-less-than-k problem

--- javascript/two-pointers/subarray-product-less-than-k.js
+++ javascript/two-pointers/subarray-product-less-than-k.js
@@ -1,5 +1,20 @@
-function findSubarrays(arr) {
+function findSubarrays(arr, target) {
+  const subarrays = []
+  let product = 1;
+  let left = 0;
 
+  for (let right = 0; right < arr.length; right++) {
+    product *= arr[right];
+    while(product >= target && left <= right) {
+      product /= arr[left++];
+    }
+    const temp = [];
+    for (let i = right; i >= left; i--) {
+      temp.unshift(arr[i]);
+      subarrays.push([...temp]);
+    }
+  }
+  return subarrays;
 }
 
 console.log(findSubarrays([2, 5, 3, 10], 30))
