7731f5c5abee121783b76a7b69fbfcc8c1c1f2cd
nhanluongoe <nhanlt18@gmail.com>
2022-09-03 16:32:27 +0700

    challenge(java): solve find-middle-linkedlist problem

--- java/fast-slow-pointers/MiddleOfLinkedList.java
+++ java/fast-slow-pointers/MiddleOfLinkedList.java
@@ -9,7 +9,14 @@ class ListNode {
 
 public class MiddleOfLinkedList {
   public static ListNode findMiddle(ListNode head) {
+    ListNode slow = head, fast = head;
 
+    while(fast != null && fast.next != null) {
+      slow = slow.next;
+      fast = fast.next.next;
+    }
+
+    return slow;
   }
 
   public static void main(String[] args) {
