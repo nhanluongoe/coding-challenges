eb8ee8adcfd5b70be8db4f92895940d6a1fbb603
nhanluongoe <nhanlt18@gmail.com>
2022-09-03 14:45:22 +0700

    challenge(java): solve happy-number

--- java/fast-slow-pointers/HappyNumber.java
+++ java/fast-slow-pointers/HappyNumber.java
@@ -1,8 +1,27 @@
 public class HappyNumber {
   public static boolean find(int number) {
+    int slow = number, fast = number;
 
-    return false;
+    do {
+      slow = sumOfSquareOfDigits(slow);
+      fast = sumOfSquareOfDigits(sumOfSquareOfDigits(fast));
+    } while (slow != fast);
+
+    return slow == 1;
   }
+
+  public static int sumOfSquareOfDigits(int number) {
+    int result = 0;
+
+    while (number > 0) {
+      int lastDigit = number % 10;
+      result += lastDigit * lastDigit;
+      number /= 10;
+    }
+
+    return result;
+  }
+
   public static void main(String[] args) {
     System.out.println(find(23));
     System.out.println(find(12));
