0ae6092c6676810bbb2134a11e8b54b2293b2ffc
nhanluongoe <nhanlt18@gmail.com>
2022-09-04 15:58:15 +0700

    challenge(java): solve insert-interval problem

--- java/merge-intervals/InsertInterval.java
+++ java/merge-intervals/InsertInterval.java
@@ -15,7 +15,34 @@ class Interval {
  */
 public class InsertInterval {
   public static List<Interval> insert(List<Interval> intervals, Interval newInterval) {
+    intervals.add(newInterval);
+    return merge(intervals);
+  }
 
+  public static List<Interval> merge(List<Interval> intervals) {
+    Collections.sort(intervals, (interval1, interval2) -> Integer.compare(interval1.start, interval2.start));
+
+    List<Interval> mergedIntervals = new LinkedList<>();
+    Iterator<Interval> intervalIterator = intervals.iterator();
+    Interval interval = intervalIterator.next();
+    int start = interval.start;
+    int end = interval.end;
+
+    while (intervalIterator.hasNext()) {
+      interval = intervalIterator.next();
+      if (interval.start < end) {
+        end = Math.max(interval.end, end);
+      } else {
+        mergedIntervals.add(new Interval(start, end));
+        start = interval.start;
+        end = interval.end;
+      }
+    }
+
+    // Add the last interval
+    mergedIntervals.add(new Interval(start, end));
+
+    return mergedIntervals;
   }
 
   public static void main(String[] args) {
@@ -31,9 +58,9 @@ public class InsertInterval {
     System.out.println();
 
     input = new ArrayList<>();
-    input.add(new Interval(6, 7));
-    input.add(new Interval(2, 4));
-    input.add(new Interval(5, 9));
+    input.add(new Interval(1, 3));
+    input.add(new Interval(5, 7));
+    input.add(new Interval(8, 12));
     System.out.println("Merged intervals: ");
     for (Interval interval : insert(input, new Interval(4, 10))) {
       System.out.print("[" + interval.start + ", " + interval.end + "]");
