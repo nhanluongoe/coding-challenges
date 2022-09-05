2b1ac495d021121994c5154ce30df49eb1c1a616
nhanluongoe <nhanlt18@gmail.com>
2022-09-05 13:30:14 +0700

    challenge(java): add problem statement

--- java/cyclic-sort/MissingNumber.java
+++ java/cyclic-sort/MissingNumber.java
@@ -1,5 +1,10 @@
 import java.util.*;
 
+/**
+ * Problem statement: We are given an array containing ‘n’ distinct numbers
+ * taken from the range 0 to ‘n’. Since the array has only ‘n’ numbers out of
+ * the total ‘n+1’ numbers, find the missing number.
+ */
 public class MissingNumber {
   public static int findMissingNumber(int[] nums) {
 
@@ -7,10 +12,10 @@ public class MissingNumber {
   }
 
   public static void main(String[] args) {
-    int[] arr1 = {4, 0, 3, 1};
+    int[] arr1 = { 4, 0, 3, 1 };
     System.out.println("Missing number: " + findMissingNumber(arr1));
 
-    int[] arr2 = {8, 3, 5, 2, 4, 6, 0, 1};
+    int[] arr2 = { 8, 3, 5, 2, 4, 6, 0, 1 };
     System.out.println("Missing number: " + findMissingNumber(arr2));
-  }  
+  }
 }
