b66307a4983b39c62a92f19800a24c24de3d4ffa
nhanluongoe <nhanlt18@gmail.com>
2022-09-05 13:28:27 +0700

    challenge(java): init missing-number problem

---
+++ java/cyclic-sort/MissingNumber.java
@@ -0,0 +1,16 @@
+import java.util.*;
+
+public class MissingNumber {
+  public static int findMissingNumber(int[] nums) {
+
+    return -1;
+  }
+
+  public static void main(String[] args) {
+    int[] arr1 = {4, 0, 3, 1};
+    System.out.println("Missing number: " + findMissingNumber(arr1));
+
+    int[] arr2 = {8, 3, 5, 2, 4, 6, 0, 1};
+    System.out.println("Missing number: " + findMissingNumber(arr2));
+  }  
+}
