5a1d4f1d4553075d69a0afc66916b5c8e64cc5fa
nhanluongoe <nhanlt18@gmail.com>
2022-09-06 17:32:25 +0700

    challenge(java): solve reverse-linkedlist problem

--- java/inplace-reversal-linkedlist/ReverseLinkedList.java
+++ java/inplace-reversal-linkedlist/ReverseLinkedList.java
@@ -16,7 +16,18 @@ class ListNode {
  */
 public class ReverseLinkedList {
   public static ListNode reverse(ListNode head) {
+    ListNode current = head;
+    ListNode previous = null;
+    ListNode next = null;
 
+    while (current != null) {
+      next = current.next;
+      current.next = previous;
+      previous = current;
+      current = next;
+    }
+
+    return previous;
   }
 
   public static void main(String[] args) {
