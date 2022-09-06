39073d4aca0bd5956c0472009a98c7e11a112bf4
nhanluongoe <nhanlt18@gmail.com>
2022-09-06 21:13:49 +0700

    challenge(java): solve reverse-sub-list problem

--- java/inplace-reversal-linkedlist/ReverseSubList.java
+++ java/inplace-reversal-linkedlist/ReverseSubList.java
@@ -13,6 +13,36 @@ class ListNode {
  */
 public class ReverseSubList {
   public static ListNode reverse(ListNode head, int p, int q) {
+    if (p == q) return head;
+
+    ListNode current = head, previous = null;
+    // skipping (p - 1) first nodes, current will point to "p-th" node
+    for (int i = 0; current != null && i < p - 1; i++) {
+      previous = current;
+      current = current.next;
+    }
+
+    ListNode LastNodeOfFirstPart = previous;
+    ListNode LastNodeOfSubList = current;
+    ListNode next = null;
+    // reverse node between p and q
+    for (int i = 0; i < q - p + 1; i++) {
+      next = current.next;
+      current.next = previous;
+      previous = current;
+      current = next;
+    }
+
+    // connect to the first part
+    if (LastNodeOfFirstPart != null) {
+      LastNodeOfFirstPart.next = previous;
+    } else {
+      head = previous;
+    }
+
+    // connect to the second second part 
+    LastNodeOfSubList.next = current;
+
 
     return head;
   }
