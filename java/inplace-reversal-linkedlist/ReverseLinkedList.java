f272984e5bb8b628daaa3dc217c49b48ecab1036
nhanluongoe <nhanlt18@gmail.com>
2022-09-06 17:09:57 +0700

    challenge(java): add problem statement

--- java/inplace-reversal-linkedlist/ReverseLinkedList.java
+++ java/inplace-reversal-linkedlist/ReverseLinkedList.java
@@ -9,6 +9,11 @@ class ListNode {
   }
 }
 
+/**
+ * Problem statement: Given the head of a Singly LinkedList, reverse the
+ * LinkedList. Write a function to return the new head of the reversed
+ * LinkedList.
+ */
 public class ReverseLinkedList {
   public static ListNode reverse(ListNode head) {
 
@@ -26,5 +31,5 @@ public class ReverseLinkedList {
       System.out.print(result.value + " ");
       result = result.next;
     }
-  } 
+  }
 }
