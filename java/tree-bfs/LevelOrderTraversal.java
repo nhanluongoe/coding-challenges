c3f5f204d2cefaa1693e3bfe5c9f8697b949701b
nhanluongoe <nhanlt18@gmail.com>
2022-09-07 21:22:01 +0700

    challenge(java): solve level-order-traversal problem

--- java/tree-bfs/LevelOrderTraversal.java
+++ java/tree-bfs/LevelOrderTraversal.java
@@ -17,7 +17,25 @@ class TreeNode {
  */
 public class LevelOrderTraversal {
   public static List<List<Integer>> traverse(TreeNode root) {
+    List<List<Integer>> result = new ArrayList<>();
 
+    Queue<TreeNode> queue = new LinkedList<>();
+    queue.offer(root);
+    while (!queue.isEmpty()) {
+      int levelSize = queue.size();
+      List<Integer> currentLevel = new ArrayList<>(levelSize);
+      for (int i = 0; i < levelSize; i++) {
+        TreeNode currentNode = queue.poll();
+        currentLevel.add(currentNode.value);
+        if (currentNode.left != null)
+          queue.offer(currentNode.left);
+        if (currentNode.right != null)
+          queue.offer(currentNode.right);
+      }
+      result.add(currentLevel);
+    }
+
+    return result;
   }
 
   public static void main(String[] args) {
