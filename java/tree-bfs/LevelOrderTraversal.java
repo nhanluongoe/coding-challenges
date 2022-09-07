cc46653a0ace7140451c2d682284e3a9b967402a
nhanluongoe <nhanlt18@gmail.com>
2022-09-07 21:00:30 +0700

    challenge(java): init level-order-traversal problem

---
+++ java/tree-bfs/LevelOrderTraversal.java
@@ -0,0 +1,27 @@
+import java.util.*;
+
+class TreeNode {
+  int value = 0;
+  TreeNode left;
+  TreeNode right;
+
+  public TreeNode(int value) {
+    this.value = value;
+  }
+}
+
+public class LevelOrderTraversal {
+  public static List<List<Integer>> traverse(TreeNode root) {
+
+  }
+  public static void main(String[] args) {
+    TreeNode root = new TreeNode(12);
+    root.left = new TreeNode(7);
+    root.right = new TreeNode(1);
+    root.left.left = new TreeNode(9);
+    root.right.left = new TreeNode(10);
+    root.right.right = new TreeNode(5);
+    List<List<Integer>> result = traverse(root);
+    System.out.println(result);
+  } 
+}
