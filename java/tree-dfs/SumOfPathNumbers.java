ee02f21adde94c1dd49cd692cb7a4210f75e6dd9
nhanluongoe <nhanlt18@gmail.com>
2022-09-11 22:09:26 +0700

    challenge(java): solve sum-of-path-numbers problem

--- java/tree-dfs/SumOfPathNumbers.java
+++ java/tree-dfs/SumOfPathNumbers.java
@@ -18,7 +18,36 @@ class TreeNode {
 public class SumOfPathNumbers {
   public static int findSumOfPathNumbers(TreeNode root) {
 
-    return 0;
+    List<List<Integer>> allPaths = new ArrayList<>();
+    List<Integer> currentPath = new ArrayList<>();
+    findPathRecursive(root, currentPath, allPaths);
+
+    int sum = 0;
+    for (List<Integer> path : allPaths) {
+      for (int i = 0; i < path.size(); i++) {
+        sum += path.get(i) * Math.pow(10, path.size() - i - 1);
+      }
+    }
+
+    System.out.println(allPaths);
+
+    return sum;
+  }
+
+  public static void findPathRecursive(TreeNode currentNode, List<Integer> currentPath, List<List<Integer>> allPaths) {
+    if (currentNode == null)
+      return;
+
+    currentPath.add(currentNode.value);
+
+    if (currentNode.left == null && currentNode.right == null) {
+      allPaths.add(new ArrayList<>(currentPath));
+    } else {
+      findPathRecursive(currentNode.left, currentPath, allPaths);
+      findPathRecursive(currentNode.right, currentPath, allPaths);
+    }
+
+    currentPath.remove(currentPath.size() - 1);
   }
 
   public static void main(String[] args) {
