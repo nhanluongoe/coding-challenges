4ce19b2b65652267d1dad01db0de7f29272543ae
nhanluongoe <nhanlt18@gmail.com>
2022-09-14 15:41:56 +0700

    challenge(java): add explanation

--- java/subsets/Subsets.java
+++ java/subsets/Subsets.java
@@ -11,10 +11,13 @@ public class Subsets {
    */
   public static List<List<Integer>> findSubsets(int[] nums) {
     List<List<Integer>> subsets = new ArrayList<>();
+    // start by adding an empty subset
     subsets.add(new ArrayList<>());
     for (int num : nums) {
+      // insert new number to all existing subsets to create new subsets
       int n = subsets.size();
       for (int i = 0; i < n; i++) {
+        // create new subset from the existing subset and insert the current number to it
         List<Integer> set = new ArrayList<>(subsets.get(i));
         set.add(num);
         subsets.add(set);
