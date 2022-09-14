54580f8e7c2831193319d430c1b70b060ee9c36d
nhanluongoe <nhanlt18@gmail.com>
2022-09-14 20:27:42 +0700

    challenge(java): init permutations problem

---
+++ java/subsets/Permutations.java
@@ -0,0 +1,17 @@
+import java.util.*;
+
+/**
+ * Problem statement: Given a set of distinct numbers, find all of its permutations.
+ */
+public class Permutations {
+  public static List<List<Integer>> findPermutations(int[] nums) {
+    List<List<Integer>> result = new ArrayList<>();
+
+    return result;
+  }
+
+  public static void main(String[] args) {
+    List<List<Integer>> result = findPermutations(new int[]{1, 3, 5});
+    System.out.println("Permutations are: " + result);
+  }
+}
