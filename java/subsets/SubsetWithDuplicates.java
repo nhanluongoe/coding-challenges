4533830906df71b8dd79a6f0f9831924d87177a8
nhanluongoe <nhanlt18@gmail.com>
2022-09-14 16:21:00 +0700

    challenge(java): solve subset-with-duplicates problem

--- java/subsets/SubsetWithDuplicates.java
+++ java/subsets/SubsetWithDuplicates.java
@@ -6,7 +6,21 @@ import java.util.*;
  */
 public class SubsetWithDuplicates {
   public static List<List<Integer>> findSubsets(int[] nums) {
+    Arrays.sort(nums);
     List<List<Integer>> subsets = new ArrayList<>();
+    subsets.add(new ArrayList<>());
+    for (int i = 0; i < nums.length; i++) {
+      int n = subsets.size();
+      int startIndex = 0;
+      if (i > 0 && nums[i] == nums[i - 1])
+        startIndex = (int) Math.pow(2, i - 1);
+      for (int j = startIndex; j < n; j++) {
+        List<Integer> set = new ArrayList<>(subsets.get(j));
+        set.add(nums[i]);
+        subsets.add(set);
+      }
+
+    }
 
     return subsets;
   }
