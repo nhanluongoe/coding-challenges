3d2528a9f2078eb66f14e16faf90f4934b36c812
nhanluongoe <nhanlt18@gmail.com>
2022-09-14 21:01:50 +0700

    challenge(java): solve permutations problem

--- java/subsets/Permutations.java
+++ java/subsets/Permutations.java
@@ -1,17 +1,34 @@
 import java.util.*;
 
 /**
- * Problem statement: Given a set of distinct numbers, find all of its permutations.
+ * Problem statement: Given a set of distinct numbers, find all of its
+ * permutations.
  */
 public class Permutations {
   public static List<List<Integer>> findPermutations(int[] nums) {
     List<List<Integer>> result = new ArrayList<>();
+    Queue<List<Integer>> permutations = new LinkedList<>();
+    permutations.offer(new ArrayList<>());
+    for (int num : nums) {
+      int n = permutations.size();
+      for (int i = 0; i < n; i++) {
+        List<Integer> oldPermutations = permutations.poll();
+        for (int j = 0; j <= oldPermutations.size(); j++) {
+          List<Integer> newPermutations = new ArrayList<>(oldPermutations);
+          newPermutations.add(j, num);
+          if (newPermutations.size() == nums.length)
+            result.add(newPermutations);
+          else
+            permutations.offer(newPermutations);
+        }
+      }
+    }
 
     return result;
   }
 
   public static void main(String[] args) {
-    List<List<Integer>> result = findPermutations(new int[]{1, 3, 5});
+    List<List<Integer>> result = findPermutations(new int[] { 1, 3, 5 });
     System.out.println("Permutations are: " + result);
   }
 }
