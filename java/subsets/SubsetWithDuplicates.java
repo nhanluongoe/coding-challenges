1cd2fd1f1b399fd3334cf48f87fa634c00454cf2
nhanluongoe <nhanlt18@gmail.com>
2022-09-14 16:25:58 +0700

    challenge(java): add alternative solution

--- java/subsets/SubsetWithDuplicates.java
+++ java/subsets/SubsetWithDuplicates.java
@@ -15,14 +15,19 @@ public class SubsetWithDuplicates {
     List<List<Integer>> subsets = new ArrayList<>();
     // add an empty set to the list
     subsets.add(new ArrayList<>());
+    // int startIndex = 0, endIndex = 0;
     for (int i = 0; i < nums.length; i++) {
       // add current number to all existing subsets
       int n = subsets.size();
       int startIndex = 0;
+      // startIndex = 0;
       // if current and the previous elements are same, create new subsets only from the subsets added
       // in the previous step
       if (i > 0 && nums[i] == nums[i - 1])
         startIndex = (int) Math.pow(2, i - 1);
+        // startIndex = endIndex + 1;
+      // endIndex = subsets.size() - 1;
+      // for (int j = startIndex, j <= endIndex; j++) {
       for (int j = startIndex; j < n; j++) {
         // create new subset from the existing subset and and the current number to it
         List<Integer> set = new ArrayList<>(subsets.get(j));
