da606c6140f777ee3294c68a4e29833c55d6f127
nhanluongoe <nhanlt18@gmail.com>
2022-09-14 15:37:04 +0700

    challenge(java): solve subsets problem

--- java/subsets/Subsets.java
+++ java/subsets/Subsets.java
@@ -7,7 +7,16 @@ import java.util.*;
 public class Subsets {
   public static List<List<Integer>> findSubsets(int[] nums) {
     List<List<Integer>> subsets = new ArrayList<>();
-
+    subsets.add(new ArrayList<>());
+    for (int num : nums) {
+      int n = subsets.size();
+      for (int i = 0; i < n; i++) {
+        List<Integer> set = new ArrayList<>(subsets.get(i));
+        set.add(num);
+        subsets.add(set);
+      }
+    }
+    
     return subsets;
   }
 
