9242ea730e599d2b86d76852d07c47542f089f1b
nhanluongoe <nhanlt18@gmail.com>
2022-09-14 16:23:45 +0700

    challenge(java): add explanation

--- java/subsets/SubsetWithDuplicates.java
+++ java/subsets/SubsetWithDuplicates.java
@@ -10,15 +10,21 @@ public class SubsetWithDuplicates {
    * Space complexity: O(2^n)
    */
   public static List<List<Integer>> findSubsets(int[] nums) {
+    // sort the array to handle duplicates
     Arrays.sort(nums);
     List<List<Integer>> subsets = new ArrayList<>();
+    // add an empty set to the list
     subsets.add(new ArrayList<>());
     for (int i = 0; i < nums.length; i++) {
+      // add current number to all existing subsets
       int n = subsets.size();
       int startIndex = 0;
+      // if current and the previous elements are same, create new subsets only from the subsets added
+      // in the previous step
       if (i > 0 && nums[i] == nums[i - 1])
         startIndex = (int) Math.pow(2, i - 1);
       for (int j = startIndex; j < n; j++) {
+        // create new subset from the existing subset and and the current number to it
         List<Integer> set = new ArrayList<>(subsets.get(j));
         set.add(nums[i]);
         subsets.add(set);
