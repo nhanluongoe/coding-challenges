bf5ee862d3742657af1c742c5ae784b4b156949f
nhanluongoe <nhanlt18@gmail.com>
2022-09-15 15:48:49 +0700

    challenge(java): add better solution

--- java/subsets/LetterCaseStringPermutation.java
+++ java/subsets/LetterCaseStringPermutation.java
@@ -5,11 +5,33 @@ import java.util.*;
  * the character sequence but changing case.
  */
 public class LetterCaseStringPermutation {
+  public static List<String> findLetterCaseStringPermutations(String str) {
+    List<String> permutations = new ArrayList<>();
+    if (str == null)
+      return permutations;
+
+    permutations.add(str);
+    for (int i = 0; i < str.length(); i++) {
+      if (Character.isLetter(str.charAt(i))) {
+        int n = permutations.size();
+        for (int j = 0; j < n; j++) {
+          char[] chs = permutations.get(j).toCharArray();
+          if (Character.isUpperCase(chs[i]))
+            chs[i] = Character.toLowerCase(chs[i]);
+          else
+            chs[i] = Character.toUpperCase(chs[i]);
+          permutations.add(String.valueOf(chs));
+        }
+      }
+    }
+    return permutations;
+  }
+
   /**
    * Time complexity: O(n * 2^n)
    * Space complexity: o(n * 2^n)
    */
-  public static List<String> findLetterCaseStringPermutations(String str) {
+  public static List<String> findLetterCaseStringPermutations1(String str) {
     List<String> permutations = new ArrayList<>();
     if (str == null)
       return permutations;
