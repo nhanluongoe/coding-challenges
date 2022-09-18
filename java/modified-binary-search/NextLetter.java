ebc06b493f69d91eb1a229fefb4c1c380b5405a5
nhanluongoe <nhanlt18@gmail.com>
2022-09-18 21:15:42 +0700

    java(challenge): add problem statement

--- java/modified-binary-search/NextLetter.java
+++ java/modified-binary-search/NextLetter.java
@@ -1,13 +1,25 @@
+/**
+ * Problem statement: Given an array of lowercase letters sorted in ascending
+ * order, find the smallest letter in the given array greater than a given
+ * ‘key’.
+ * 
+ * Assume the given array is a circular list, which means that the last letter
+ * is assumed to be connected with the first letter. This also means that the
+ * smallest letter in the given array is greater than the last letter of the
+ * array and is also the first letter of the array.
+ * 
+ * Write a function to return the next letter of the given ‘key’.
+ */
 public class NextLetter {
   public static char searchNextLetter(char[] letters, char key) {
 
     return letters[0];
   }
-  
+
   public static void main(String[] args) {
-    System.out.println(searchNextLetter(new char[]{'a', 'c', 'f', 'h'}, 'f'));
-    System.out.println(searchNextLetter(new char[]{'a', 'c', 'f', 'h'}, 'b'));
-    System.out.println(searchNextLetter(new char[]{'a', 'c', 'f', 'h'}, 'm'));
-    System.out.println(searchNextLetter(new char[]{'a', 'c', 'f', 'h'}, 'h'));
+    System.out.println(searchNextLetter(new char[] { 'a', 'c', 'f', 'h' }, 'f'));
+    System.out.println(searchNextLetter(new char[] { 'a', 'c', 'f', 'h' }, 'b'));
+    System.out.println(searchNextLetter(new char[] { 'a', 'c', 'f', 'h' }, 'm'));
+    System.out.println(searchNextLetter(new char[] { 'a', 'c', 'f', 'h' }, 'h'));
   }
 }
