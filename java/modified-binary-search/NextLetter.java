d3141d88eac7f62d2a0650414a9f6bed3e8fb8c3
nhanluongoe <nhanlt18@gmail.com>
2022-09-18 21:30:32 +0700

    challenge(java): solve next-letter problem

--- java/modified-binary-search/NextLetter.java
+++ java/modified-binary-search/NextLetter.java
@@ -12,8 +12,25 @@
  */
 public class NextLetter {
   public static char searchNextLetter(char[] letters, char key) {
+    int start = 0;
+    int end = letters.length - 1;
+    while (start <= end) {
+      int middle = start + (end - start) / 2;
 
-    return letters[0];
+      if (letters[middle] < key) {
+        start = middle + 1;
+      } else if (letters[middle] > key) {
+        end = middle - 1;
+      } else {
+        if (middle + 1 > letters.length - 1)
+          return letters[0];
+        return letters[middle + 1];
+      }
+    }
+
+    if (start + 1 > letters.length - 1)
+      return letters[0];
+    return letters[start];
   }
 
   public static void main(String[] args) {
