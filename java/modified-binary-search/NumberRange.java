8699adf7bbf21ba13eca389bfe09ae4e3cc5432c
nhanluongoe <nhanlt18@gmail.com>
2022-09-19 18:12:14 +0700

    challenge(java): add better solution for range-number problem

--- java/modified-binary-search/NumberRange.java
+++ java/modified-binary-search/NumberRange.java
@@ -36,12 +36,46 @@ public class NumberRange {
     return result;
   }
 
+  public static int[] findRangeBetter(int[] arr, int key) {
+    int[] result = { -1, -1 };
+    result[0] = search(arr, key, false);
+    if (result[0] != -1) {
+      result[1] = search(arr, key, true);
+    }
+
+    return result;
+  }
+
+  public static int search(int[] arr, int key, boolean findMaxIndex) {
+    int n = arr.length;
+    int keyIndex = -1;
+    int start = 0, end = n - 1;
+    while (start <= end) {
+      int middle = start + (end - start) / 2;
+      if (arr[middle] < key) {
+        start = middle + 1;
+      } else if (arr[middle] > key) {
+        end = middle - 1;
+      } else {
+        keyIndex = middle;
+        if (findMaxIndex) {
+          start = middle + 1;
+        } else {
+          end = middle - 1;
+        }
+      }
+
+    }
+
+    return keyIndex;
+  }
+
   public static void main(String[] args) {
-    int[] result = findRange(new int[] { 4, 6, 6, 6, 9 }, 6);
+    int[] result = findRangeBetter(new int[] { 4, 6, 6, 6, 9 }, 6);
     System.out.println("Range: [" + result[0] + ", " + result[1] + "]");
-    result = findRange(new int[] { 1, 3, 8, 10, 15 }, 10);
+    result = findRangeBetter(new int[] { 1, 3, 8, 10, 15 }, 10);
     System.out.println("Range: [" + result[0] + ", " + result[1] + "]");
-    result = findRange(new int[] { 1, 3, 8, 10, 15 }, 12);
+    result = findRangeBetter(new int[] { 1, 3, 8, 10, 15 }, 12);
     System.out.println("Range: [" + result[0] + ", " + result[1] + "]");
   }
 }
