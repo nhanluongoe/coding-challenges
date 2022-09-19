53e9d83417393e3c4b94fbf2d852d95f43db3eaf
nhanluongoe <nhanlt18@gmail.com>
2022-09-19 17:58:39 +0700

    challenge(jav): solve range-number problem

--- java/modified-binary-search/NumberRange.java
+++ java/modified-binary-search/NumberRange.java
@@ -8,17 +8,36 @@
  */
 public class NumberRange {
   public static int[] findRange(int[] arr, int key) {
-    int[] result = {-1, -1};
+    int[] result = { -1, -1 };
+
+    int n = arr.length;
+    int start = 0, end = n - 1;
+    while (start <= end) {
+      int middle = start + (end - start) / 2;
+      if (arr[middle] < key) {
+        start = middle + 1;
+      } else if (arr[middle] > key) {
+        end = middle - 1;
+      } else {
+        int i = middle, j = middle;
+        while (arr[i] == key)
+          i--;
+        while (arr[j] == key)
+          j++;
+
+        return new int[] { ++i, --j };
+      }
+    }
 
     return result;
   }
 
   public static void main(String[] args) {
-    int[ ] result = findRange(new int[] {4, 6, 6, 6, 9}, 6);
+    int[] result = findRange(new int[] { 4, 6, 6, 6, 9 }, 6);
     System.out.println("Range: [" + result[0] + ", " + result[1] + "]");
-    result = findRange(new int[] {1, 3, 8, 10, 15}, 10);
+    result = findRange(new int[] { 1, 3, 8, 10, 15 }, 10);
     System.out.println("Range: [" + result[0] + ", " + result[1] + "]");
-    result = findRange(new int[] {1, 3, 8, 10, 15}, 12);
+    result = findRange(new int[] { 1, 3, 8, 10, 15 }, 12);
     System.out.println("Range: [" + result[0] + ", " + result[1] + "]");
   }
 }
