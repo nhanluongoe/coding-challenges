4e104b3a38d8b0f099d9bd73253e701f03ec03ed
nhanluongoe <nhanlt18@gmail.com>
2022-09-19 20:28:48 +0700

    challenge(java): solve search-infinite-sorted-array problem

--- java/modified-binary-search/SearchInfiniteSortedArray.java
+++ java/modified-binary-search/SearchInfiniteSortedArray.java
@@ -25,7 +25,25 @@ class ArrayReader {
  */
 public class SearchInfiniteSortedArray {
   public static int search(ArrayReader reader, int key) {
+    int start = 0, end = 1;
+    int boundSize = end - start + 1;
 
+    while (key > reader.get(end)) {
+      boundSize *= 2;
+      start = end + 1;
+      end = start + boundSize - 1;
+    }
+
+    while (start <= end) {
+      int middle = start + (end - start)/2;
+      if (reader.get(middle) > key) {
+        end = middle - 1;
+      } else if (reader.get(middle) < key) {
+        start = middle + 1;
+      } else {
+        return middle;
+      }
+    }
     return -1;
   }
 
@@ -33,8 +51,8 @@ public class SearchInfiniteSortedArray {
     ArrayReader reader = new ArrayReader(new int[] { 4, 6, 8, 10, 12, 14, 16, 18, 20, 22, 24, 26, 28, 30 });
     System.out.println(search(reader, 16));
     System.out.println(search(reader, 11));
-    reader = new ArrayReader(new int[] { 1, 3, 8, 10, 15 });
-    System.out.println(search(reader, 15));
-    System.out.println(search(reader, 200));
+    // reader = new ArrayReader(new int[] { 1, 3, 8, 10, 15 });
+    // System.out.println(search(reader, 15));
+    // System.out.println(search(reader, 200));
   }
 }
