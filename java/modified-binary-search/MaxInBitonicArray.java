fde820b477f76b6bd929d6500a6abd682ad9cbee
nhanluongoe <nhanlt18@gmail.com>
2022-09-20 14:35:10 +0700

    challenge(java): solve max-in-bitonic-array problem

--- java/modified-binary-search/MaxInBitonicArray.java
+++ java/modified-binary-search/MaxInBitonicArray.java
@@ -6,8 +6,17 @@
  */
 public class MaxInBitonicArray {
   public static int findMax(int[] arr) {
+    int start = 0, end = arr.length - 1;
+    while (start < end) {
+      int middle = start + (end - start) / 2;
+      if (arr[middle] < arr[middle + 1]) {
+        start = middle + 1;
+      } else {
+        end = middle;
+      }
+    }
 
-    return -1;
+    return arr[start];
   }
 
   public static void main(String[] args) {
