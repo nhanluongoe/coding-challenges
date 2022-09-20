568268902a877b579b221d2dc6a762156d9005fa
nhanluongoe <nhanlt18@gmail.com>
2022-09-20 14:53:37 +0700

    challenge(java): solve k-largest-numbers problem

--- java/top-k-elements/KLargestNumbers.java
+++ java/top-k-elements/KLargestNumbers.java
@@ -3,11 +3,23 @@ import java.util.*;
 /**
  * Problem statement: Given an unsorted array of numbers, find the ‘K’ largest
  * numbers in it.
+ * 
+ * Can we do better than O(n*logn) by sorting the array first?
  */
 public class KLargestNumbers {
   public static List<Integer> findKLargestNumbers(int[] nums, int k) {
+    PriorityQueue<Integer> minHeap = new PriorityQueue<>((a, b) -> a - b);
+    for (int i = 0; i < k; i++) minHeap.offer(nums[i]);
 
-    return new ArrayList<>();
+    for (int i = k; i < nums.length; i++) {
+      int num = nums[i];
+      if (num > minHeap.peek()) {
+        minHeap.poll();
+        minHeap.offer(num);
+      }
+    }
+
+    return new ArrayList<>(minHeap);
   }
 
   public static void main(String[] args) {
