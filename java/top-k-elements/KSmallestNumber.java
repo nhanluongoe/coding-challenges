4947e15bb1a1b345fccfe6fcb9b286d32cf3e752
nhanluongoe <nhanlt18@gmail.com>
2022-09-21 09:50:04 +0700

    java(challenge): init kth-smallest-number problem

---
+++ java/top-k-elements/KSmallestNumber.java
@@ -0,0 +1,26 @@
+import java.util.*;
+
+/**
+ * Problem statement: Given an unsorted array of numbers, find Kth smallest
+ * number in it.
+ * 
+ * Please note that it is the Kth smallest number in the sorted order, not the
+ * Kth distinct element.
+ */
+public class KSmallestNumber {
+  public static int findKthSmallestNumber(int[] nums, int k) {
+
+    return -1;
+  }
+
+  public static void main(String[] args) {
+    int result = findKthSmallestNumber(new int[] { 1, 5, 12, 2, 11, 5 }, 3);
+    System.out.println("Kth smallest number is: " + result);
+
+    result = findKthSmallestNumber(new int[] { 1, 5, 12, 2, 11, 5 }, 4);
+    System.out.println("Kth smallest number is: " + result);
+
+    result = findKthSmallestNumber(new int[] { 5, 12, 11, -1, 12 }, 3);
+    System.out.println("Kth smallest number is: " + result);
+  }
+}
