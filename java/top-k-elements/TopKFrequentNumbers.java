14ee4abb86c789b2d8914a0c8bb998dc8494a2e7
nhanluongoe <nhanlt18@gmail.com>
2022-09-22 10:12:28 +0700

    challenge(java): solve top-k-frequent-numbers problem

--- java/top-k-elements/TopKFrequentNumbers.java
+++ java/top-k-elements/TopKFrequentNumbers.java
@@ -6,7 +6,23 @@ import java.util.*;
  */
 public class TopKFrequentNumbers {
   public static List<Integer> findTopKFrequentNumbers(int[] nums, int k) {
+
+    Map<Integer, Integer> numberFrequencyMap = new HashMap<>();
+    for (int num : nums) {
+      numberFrequencyMap.put(num, numberFrequencyMap.getOrDefault(num, 0) + 1);
+    }
+
+    PriorityQueue<Map.Entry<Integer, Integer>> minHeap = new PriorityQueue<>((a, b) -> a.getValue() - b.getValue());
+    for (Map.Entry<Integer, Integer> entry : numberFrequencyMap.entrySet()) {
+      minHeap.offer(entry);
+      if (minHeap.size() > k)
+        minHeap.poll();
+    }
+
     List<Integer> topNumbers = new ArrayList<>();
+    while (!minHeap.isEmpty()) {
+      topNumbers.add(minHeap.poll().getKey());
+    }
 
     return topNumbers;
   }
