065c0fa09dd0e1cb822ee08dcb9b75e6fb2df627
nhanluongoe <nhanlt18@gmail.com>
2022-09-22 10:58:21 +0700

    challenge(java): init kth-largest-number-in-stream problem

---
+++ java/top-k-elements/KthLargestNumberInStream.java
@@ -0,0 +1,32 @@
+import java.util.*;
+
+/**
+ * Problem statement: Design a class to efficiently find the Kth largest element
+ * in a stream of numbers.
+ * 
+ * The class should have the following two things:
+ * 
+ * The constructor of the class should accept an integer array containing
+ * initial numbers from the stream and an integer ‘K’.
+ * The class should expose a function add(int num) which will store the given
+ * number and return the Kth largest number.
+ */
+public class KthLargestNumberInStream {
+  public KthLargestNumberInStream(int[] nums, int k) {
+
+  }
+
+  public int add(int num) {
+
+    return -1;
+  }
+
+  public static void main(String[] args) {
+    int[] input = { 3, 1, 5, 12, 2, 11 };
+
+    KthLargestNumberInStream kthLargestNumberInStream = new KthLargestNumberInStream(input, 4);
+    System.out.println("4th largest number is: " + kthLargestNumberInStream.add(6));
+    System.out.println("4th largest number is: " + kthLargestNumberInStream.add(13));
+    System.out.println("4th largest number is: " + kthLargestNumberInStream.add(4));
+  }
+}
