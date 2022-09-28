0902cb45c91933532527005ca9bd33e020f0d2ae
nhanluongoe <nhanlt18@gmail.com>
2022-09-28 20:53:12 +0700

    challenge(java): add time/space complexity

--- java/top-k-elements/KClosestElements.java
+++ java/top-k-elements/KClosestElements.java
@@ -1,14 +1,46 @@
 import java.util.*;
 
+class Entry {
+  int key;
+  int value;
+
+  public Entry(int key, int value) {
+    this.key = key;
+    this.value = value;
+  }
+}
+
 /**
  * Problem statement: Given a sorted number array and two integers ‘K’ and ‘X’,
  * find ‘K’ closest numbers to ‘X’ in the array. Return the numbers in the
  * sorted order. ‘X’ is not necessarily present in the array.
  */
 public class KClosestElements {
+  /**
+   * Time complexity: O(K*logK + (n - K)*logK)
+   * Space complexity: O(K)
+   */
   public static List<Integer> findClosestElements(int[] arr, int K, Integer X) {
     List<Integer> result = new ArrayList<>();
+    PriorityQueue<Entry> maxHeap = new PriorityQueue<>((a, b) -> b.value - a.value);
 
+    for (int i = 0; i < K; i++) {
+
+      maxHeap.offer(new Entry(arr[i], Math.abs(arr[i] - X)));
+    }
+
+    for (int i = K; i < arr.length; i++) {
+      if (maxHeap.peek().value > Math.abs(arr[i] - X)) {
+        maxHeap.poll();
+        maxHeap.offer(new Entry(arr[i], Math.abs(arr[i] - X)));
+      }
+    }
+
+    while (!maxHeap.isEmpty()) {
+      result.add(maxHeap.poll().key);
+    }
+
+    Collections.sort(result);
     return result;
   }
 
