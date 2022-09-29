7a523fc1228991d3dc3835009caea4bcd08cec63
nhanluongoe <nhanlt18@gmail.com>
2022-09-29 21:18:56 +0700

    challenge(java): add better solution for sum-of-elements

--- java/top-k-elements/SumOfElements.java
+++ java/top-k-elements/SumOfElements.java
@@ -39,11 +39,27 @@ public class SumOfElements {
     return maxHeap.peek();
   }
 
+  public static int findSumOfElementsBetter(int[] nums, int k1, int k2) {
+    PriorityQueue<Integer> minHeap = new PriorityQueue<>((a, b) -> a - b);
+
+    for (int num : nums)
+      minHeap.offer(num);
+
+    for (int i = 0; i < k1; i++)
+      minHeap.poll();
+
+    int sum = 0;
+    for (int i = 0; i < k2 - k1 - 1; i++)
+      sum += minHeap.poll();
+
+    return sum;
+  }
+
   public static void main(String[] args) {
-    int result = findSumOfElements(new int[] { 1, 3, 12, 5, 15, 11 }, 3, 6);
+    int result = findSumOfElementsBetter(new int[] { 1, 3, 12, 5, 15, 11 }, 3, 6);
     System.out.println(result);
 
-    result = findSumOfElements(new int[] { 3, 5, 8, 7 }, 1, 4);
+    result = findSumOfElementsBetter(new int[] { 3, 5, 8, 7 }, 1, 4);
     System.out.println(result);
   }
 }
