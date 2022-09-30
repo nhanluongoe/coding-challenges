f3180d91cf616aeadcfa592c31e51276f2e0f79d
nhanluongoe <nhanlt18@gmail.com>
2022-09-30 08:12:35 +0700

    challenge(java): solve rearrange-string problem

--- java/top-k-elements/RearrangeString.java
+++ java/top-k-elements/RearrangeString.java
@@ -5,14 +5,32 @@ import java.util.*;
  * such a way that no two same characters come next to each other.
  */
 public class RearrangeString {
-  public static String rearrangeString(String str) {
+  // public static String rearrangeString(String str) {
+  //   Map<Character, Integer> charFrequency = new HashMap<>();
+    for (char ch : str.toCharArray())
+      charFrequency.put(ch, charFrequency.getOrDefault(ch, 0) + 1);
 
-    return "";
+    PriorityQueue<Map.Entry<Character, Integer>> maxHeap = new PriorityQueue<>((a, b) -> b.getValue() - a.getValue());
+    for (Map.Entry<Character, Integer> entry : charFrequency.entrySet())
+      maxHeap.offer(entry);
+
+    StringBuilder result = new StringBuilder(str.length());
+    Map.Entry<Character, Integer> previousEntry = null;
+    while (!maxHeap.isEmpty()) {
+      Map.Entry<Character, Integer> entry = maxHeap.poll();
+      if (previousEntry != null && previousEntry.getValue() > 0)
+        maxHeap.offer(previousEntry);
+      result.append(entry.getKey());
+      entry.setValue(entry.getValue() - 1);
+      previousEntry = entry;
+    }
+
+    return result.length() == str.length() ? result.toString() : "";
   }
 
   public static void main(String[] args) {
     System.out.println(rearrangeString("aappp"));
     System.out.println(rearrangeString("Programming"));
-    System.out.println("aapa");
+    System.out.println(rearrangeString("aapa"));
   }
 }
