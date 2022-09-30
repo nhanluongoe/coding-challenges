ad9ef57c3a08dbcbc8d86f99dc4d3cd9da1cf1ad
nhanluongoe <nhanlt18@gmail.com>
2022-09-30 07:29:28 +0700

    challenge(java): init rearrange-string problem

---
+++ java/top-k-elements/RearrangeString.java
@@ -0,0 +1,18 @@
+import java.util.*;
+
+/**
+ * Problem statement: Given a string, find if its letters can be rearranged in
+ * such a way that no two same characters come next to each other.
+ */
+public class RearrangeString {
+  public static String rearrangeString(String str) {
+
+    return "";
+  }
+
+  public static void main(String[] args) {
+    System.out.println(rearrangeString("aappp"));
+    System.out.println(rearrangeString("Programming"));
+    System.out.println("aapa");
+  }
+}
