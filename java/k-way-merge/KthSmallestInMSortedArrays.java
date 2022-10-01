549ea8aaae5279c4eb4596b91b3cc193d5b926dc
nhanluongoe <nhanlt18@gmail.com>
2022-10-01 07:16:13 +0700

    challenge(java): solve kth-smallest-in-sorted-arrays problem

--- java/k-way-merge/KthSmallestInMSortedArrays.java
+++ java/k-way-merge/KthSmallestInMSortedArrays.java
@@ -6,18 +6,31 @@ import java.util.*;
  */
 public class KthSmallestInMSortedArrays {
   public static int findKthSmallest(List<Integer[]> lists, int k) {
+    PriorityQueue<Integer> maxHeap = new PriorityQueue<>((a, b) -> b - a);
+    for (Integer[] arr : lists) {
+      for (Integer num : arr) {
+        if (maxHeap.size() >= k) {
+          if (maxHeap.peek() > num) {
+            maxHeap.poll();
+            maxHeap.offer(num);
+          }
+        } else {
+          maxHeap.offer(num);
+        }
+      }
+    }
 
-    return -1;
+    return maxHeap.peek();
   }
 
   public static void main(String[] args) {
-    Integer[] l1 = new Integer[]{2, 6, 8};
-    Integer[] l2 = new Integer[]{3, 6, 7};
-    Integer[] l3 = new Integer[]{1, 3, 4};
+    Integer[] l1 = new Integer[] { 2, 6, 8 };
+    Integer[] l2 = new Integer[] { 3, 6, 7 };
+    Integer[] l3 = new Integer[] { 1, 3, 4 };
     List<Integer[]> lists = new ArrayList<>();
     lists.add(l1);
     lists.add(l2);
-    lists.add(l3)
+    lists.add(l3);
     int result = findKthSmallest(lists, 5);
     System.out.println("Kth smallest number is: " + result);
   }
