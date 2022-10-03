941952a190f7c767bbe736ea497d08233751f162
nhanluongoe <nhanlt18@gmail.com>
2022-10-03 07:24:18 +0700

    challenge(java): solve knapsack problem using recursive and dp

--- java/0-1-knapsack/Knapsack.java
+++ java/0-1-knapsack/Knapsack.java
@@ -29,12 +29,40 @@ public class Knapsack {
     return Math.max(profit1, profit2);
   }
 
+  public static int solveKnapsackDP(int[] profits, int[] weights, int capacity) {
+    Integer[][] dp = new Integer[profits.length][capacity + 1];
+    return knapsackRecursiveDP(profits, weights, capacity, 0, dp);
+  }
+
+  public static int knapsackRecursiveDP(int[] profits, int[] weights, int capacity, int currentIndex, Integer[][] dp) {
+    if (capacity <= 0 || currentIndex >= profits.length)
+      return 0;
+
+    if (dp[currentIndex][capacity] != null)
+      return dp[currentIndex][capacity];
+
+    int profit1 = 0;
+    if (weights[currentIndex] <= capacity)
+      profit1 = profits[currentIndex]
+          + knapsackRecursiveDP(profits, weights, capacity - weights[currentIndex], currentIndex + 1, dp);
+
+    int profit2 = knapsackRecursiveDP(profits, weights, capacity, currentIndex + 1, dp);
+
+    dp[currentIndex][capacity] = Math.max(profit1, profit2);
+    return dp[currentIndex][capacity];
+  }
+
   public static void main(String[] args) {
     int[] profits = { 1, 6, 10, 16 };
     int[] weights = { 1, 2, 3, 5 };
     int maxProfit = solveKnapsack(profits, weights, 7);
     System.out.println("Total knapsack profit: " + maxProfit);
     maxProfit = solveKnapsack(profits, weights, 6);
+    System.out.println("Total knapsack profit: " + maxProfit);
+
+    maxProfit = solveKnapsackDP(profits, weights, 7);
+    System.out.println("Total knapsack profit: " + maxProfit);
+    maxProfit = solveKnapsackDP(profits, weights, 6);
     System.out.println("Total knapsack profit: " + maxProfit);
   }
 }
