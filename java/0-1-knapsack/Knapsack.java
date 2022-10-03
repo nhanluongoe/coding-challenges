9349e96c6ecc490293f3fffde8b48e9ac1eb557e
nhanluongoe <nhanlt18@gmail.com>
2022-10-03 07:59:10 +0700

    challenge(java): solve knapsack problem using dp

--- java/0-1-knapsack/Knapsack.java
+++ java/0-1-knapsack/Knapsack.java
@@ -56,6 +56,34 @@ public class Knapsack {
     return dp[currentIndex][capacity];
   }
 
+  public static int solveKnapsackDPOnly(int[] profits, int[] weights, int capacity) {
+    if (capacity <= 0 || profits.length == 0 || profits.length != weights.length)
+      return 0;
+
+    int n = profits.length;
+    int[][] dp = new int[n][capacity + 1];
+
+    for (int i = 0; i < n; i++)
+      dp[i][0] = 0;
+
+    for (int c = 0; c <= capacity; c++)
+      if (weights[0] <= c)
+        dp[0][c] = profits[0];
+
+    for (int i = 1; i < n; i++) {
+      for (int c = 1; c <= capacity; c++) {
+        int profit1 = 0;
+        if (weights[i] <= c) {
+          profit1 = profits[i] + dp[i - 1][c - weights[i]];
+        }
+        int profit2 = dp[i - 1][c];
+        dp[i][c] = Math.max(profit1, profit2);
+      }
+    }
+
+    return dp[profits.length - 1][capacity];
+  }
+
   public static void main(String[] args) {
     int[] profits = { 1, 6, 10, 16 };
     int[] weights = { 1, 2, 3, 5 };
@@ -68,5 +96,10 @@ public class Knapsack {
     System.out.println("Total knapsack profit: " + maxProfit);
     maxProfit = solveKnapsackDP(profits, weights, 6);
     System.out.println("Total knapsack profit: " + maxProfit);
+
+    maxProfit = solveKnapsackDPOnly(profits, weights, 7);
+    System.out.println("Total knapsack profit: " + maxProfit);
+    // maxProfit = solveKnapsackDPOnly(profits, weights, 6);
+    // System.out.println("Total knapsack profit: " + maxProfit);
   }
 }
