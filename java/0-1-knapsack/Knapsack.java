2d3d96ab89bd12c69a43e227965fe65031577086
nhanluongoe <nhanlt18@gmail.com>
2022-10-03 07:06:06 +0700

    challenge(java): solve knapsack problem by recursive

--- java/0-1-knapsack/Knapsack.java
+++ java/0-1-knapsack/Knapsack.java
@@ -8,13 +8,26 @@
  */
 public class Knapsack {
   public static int solveKnapsack(int[] profits, int[] weights, int capacity) {
+    return knapsackRecursive(profits, weights, capacity, 0);
+  }
 
-    return -1;
+  public static int knapsackRecursive(int[] profits, int[] weights, int capacity, int currentIndex) {
+    if (capacity <= 0 || currentIndex >= profits.length)
+      return 0;
+
+    int profit1 = 0;
+    if (weights[currentIndex] <= capacity)
+      profit1 = profits[currentIndex]
+          + knapsackRecursive(profits, weights, capacity - weights[currentIndex], currentIndex + 1);
+
+    int profit2 = knapsackRecursive(profits, weights, capacity, currentIndex + 1);
+
+    return Math.max(profit1, profit2);
   }
 
   public static void main(String[] args) {
     int[] profits = { 1, 6, 10, 16 };
-    int[] weights = { 1, 2, 3, 55 };
+    int[] weights = { 1, 2, 3, 5 };
     int maxProfit = solveKnapsack(profits, weights, 7);
     System.out.println("Total knapsack profit: " + maxProfit);
     maxProfit = solveKnapsack(profits, weights, 6);
