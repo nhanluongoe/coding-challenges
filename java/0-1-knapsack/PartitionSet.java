c8f5cc1703fa2c8c80878089021df363d412e441
nhanluongoe <nhanlt18@gmail.com>
2022-10-04 08:48:53 +0700

    challenge(java): solve partition-set problem by top-down dynamic programming with memoization

--- java/0-1-knapsack/PartitionSet.java
+++ java/0-1-knapsack/PartitionSet.java
@@ -34,9 +34,47 @@ public class PartitionSet {
     return canPartitionRecursive(nums, sum, currentIndex + 1);
   }
 
+  public static boolean canPartitionDP(int[] nums) {
+    int sum = 0;
+    for (int num : nums) {
+      sum += num;
+    }
+
+    if (sum % 2 != 0)
+      return false;
+
+    Boolean[][] dp = new Boolean[nums.length][sum / 2 + 1];
+    return canPartitionRecursiveDP(nums, sum / 2, 0, dp);
+  }
+
+  public static boolean canPartitionRecursiveDP(int[] nums, int sum, int currentIndex, Boolean[][] dp) {
+    if (sum == 0)
+      return true;
+
+    if (nums.length == 0 || currentIndex >= nums.length)
+      return false;
+
+    if (dp[currentIndex][sum] != null)
+      return dp[currentIndex][sum];
+
+    if (nums[currentIndex] <= sum) {
+      if (canPartitionRecursiveDP(nums, sum - nums[currentIndex], currentIndex + 1, dp)) {
+        dp[currentIndex][sum] = true;
+        return dp[currentIndex][sum];
+      }
+    }
+
+    dp[currentIndex][sum] = canPartitionRecursiveDP(nums, sum, currentIndex + 1, dp);
+    return dp[currentIndex][sum];
+  }
+
   public static void main(String[] args) {
     System.out.println(canPartition(new int[] { 1, 2, 3, 4 }));
     System.out.println(canPartition(new int[] { 1, 1, 3, 4, 7 }));
     System.out.println(canPartition(new int[] { 2, 3, 4, 6 }));
+
+    System.out.println(canPartitionDP(new int[] { 1, 2, 3, 4 }));
+    System.out.println(canPartitionDP(new int[] { 1, 1, 3, 4, 7 }));
+    System.out.println(canPartitionDP(new int[] { 2, 3, 4, 6 }));
   }
 }
