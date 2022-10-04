946c1a2792c39a532f5dd8d15e61742fbf19e919
nhanluongoe <nhanlt18@gmail.com>
2022-10-04 08:12:08 +0700

    challenge(java): init can-partition problem

---
+++ java/0-1-knapsack/PartitionSet.java
@@ -0,0 +1,15 @@
+/**
+ * Problem statement: Given a set of positive numbers, find if we can partition
+ * it into two subsets such that the sum of elements in both subsets is equal.
+ */
+public class PartitionSet {
+  public static boolean canPartition(int[] num) {
+
+    return false;
+  }
+
+  public static void main(String[] args) {
+    System.out.println(canPartition(new int[] { 1, 2, 3, 4 }));
+    System.out.println(canPartition(new int[] { 1, 1, 3, 4, 7 }));
+  }
+}
