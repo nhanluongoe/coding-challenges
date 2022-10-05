92cd9c1c3c09ac01f5db1c9879a648b538ef8e17
nhanluongoe <nhanlt18@gmail.com>
2022-10-05 11:43:25 +0700

    challenge(java): solve subset-sum by bottom-up dynamic programming

--- java/0-1-knapsack/SubsetSum.java
+++ java/0-1-knapsack/SubsetSum.java
@@ -26,9 +26,40 @@ public class SubsetSum {
     return subsetExistsRecursive(nums, sum, currentIndex + 1);
   }
 
+  public static boolean subsetExistsDPBottomUp(int[] nums, int sum) {
+    boolean[][] dp = new boolean[nums.length][sum + 1];
+
+    // all subsets can make up a sum of "0"
+    for (int i = 0; i < nums.length; i++)
+      dp[i][0] = true;
+
+    // for the first subset, only the number equal to sum can make up the sum
+    for (int s = 1; s <= sum; s++)
+      dp[0][s] = (nums[0] == s ? true : false);
+
+    for (int i = 1; i < nums.length; i++) {
+      for (int s = 1; s <= sum; s++) {
+        // don't pick the number at the index
+        if (dp[i - 1][s]) {
+          dp[i][s] = dp[i - 1][s];
+          // pick the number at the index if its value is less than or qual to the sum
+          // then find a subset to get the remaining sum
+        } else if (nums[i] <= s) {
+          dp[i][s] = dp[i - 1][s - nums[i]];
+        }
+      }
+    }
+
+    return dp[nums.length - 1][sum];
+  }
+
   public static void main(String[] args) {
     System.out.println(subsetExists(new int[] { 1, 2, 3, 7 }, 6));
     System.out.println(subsetExists(new int[] { 1, 2, 7, 1, 5 }, 10));
     System.out.println(subsetExists(new int[] { 1, 3, 4, 8 }, 6));
+    System.out.println();
+    System.out.println(subsetExistsDPBottomUp(new int[] { 1, 2, 3, 7 }, 6));
+    System.out.println(subsetExistsDPBottomUp(new int[] { 1, 2, 7, 1, 5 }, 10));
+    System.out.println(subsetExistsDPBottomUp(new int[] { 1, 3, 4, 8 }, 6));
   }
 }
