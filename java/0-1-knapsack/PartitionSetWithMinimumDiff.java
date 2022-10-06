48489757e834ea82a1e5f63b879a68919ad0a476
nhanluongoe <nhanlt18@gmail.com>
2022-10-06 08:15:33 +0700

    challenge(java): solve subset-partition-with-min-diff problem using brute-force

--- java/0-1-knapsack/PartitionSetWithMinimumDiff.java
+++ java/0-1-knapsack/PartitionSetWithMinimumDiff.java
@@ -4,8 +4,23 @@
  */
 public class PartitionSetWithMinimumDiff {
   public static int canPartition(int[] nums) {
+    return canPartitionRecursive(nums, 0, 0, 0);
+  }
 
-    return -1;
+  public static int canPartitionRecursive(int[] nums, int sum1, int sum2, int currentIndex) {
+    // base check
+    if (currentIndex == nums.length)
+      return Math.abs(sum1 - sum2);
+
+    // the current number is either added to sum1 or sum2
+    // add the current number to first set
+    int diff1 = canPartitionRecursive(nums, sum1 + nums[currentIndex], sum2, currentIndex + 1);
+
+    // add the current number to second set
+    int diff2 = canPartitionRecursive(nums, sum1, sum2 + nums[currentIndex], currentIndex + 1);
+
+    // return the smaller diff
+    return Math.min(diff1, diff2);
   }
 
   public static void main(String[] args) {
