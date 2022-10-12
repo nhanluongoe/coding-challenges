c74457012cc4495901cb91a3b08e1623f24ba156
nhanluongoe <nhanlt18@gmail.com>
2022-10-12 07:46:04 +0700

    challenge(java): init all-tasks-scheduling-orders problem

---
+++ java/topological-sort/AllTasksSchedulingOrders.java
@@ -0,0 +1,27 @@
+/**
+ * Problem statement: There are ‘N’ tasks, labeled from ‘0’ to ‘N-1’. Each task
+ * can have some prerequisite tasks which need to be completed before it can be
+ * scheduled. Given the number of tasks and a list of prerequisite pairs, write
+ * a method to print all possible ordering of tasks meeting all prerequisites.
+ */
+public class AllTasksSchedulingOrders {
+  public static void; findOrders(int tasks, int[][] prerequisites) {
+
+    return;
+  }
+
+  public static void main(String[] args) {
+    List<Integer> result = AllTasksSchedulingOrders.findOrders(3,
+        new int[][] { new int[] { 0, 1 }, new int[] { 1, 2 } });
+    System.out.println(result);
+
+    result = AllTasksSchedulingOrders.findOrders(3,
+        new int[][] { new int[] { 0, 1 }, new int[] { 1, 2 }, new int[] { 2, 0 } });
+    System.out.println(result);
+
+    result = AllTasksSchedulingOrders.findOrders(6,
+        new int[][] { new int[] { 2, 5 }, new int[] { 0, 5 }, new int[] { 0, 4 },
+            new int[] { 1, 4 }, new int[] { 3, 2 }, new int[] { 1, 3 } });
+    System.out.println(result);
+  }
+}
