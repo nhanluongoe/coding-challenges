e99a17ba82cf1de0df8a3604983e69bfc2659045
nhanluongoe <nhanlt18@gmail.com>
2024-07-01 17:25:35 +0700

    challenge(javascript): implement promise-timeout

---
+++ javascript/010724-promise-timeout.js
@@ -0,0 +1,120 @@
+function promiseTimeout(promise, duration) {
+  const timeout = new Promise((_, reject) => {
+    setTimeout(() => {
+      reject('Promise timeout');
+    }, duration);
+  });
+
+  return Promise.race([promise, timeout]);
+}
+
+// Test
+describe('promiseTimeout', () => {
+  test('returns a promise', () => {
+    const promise = promiseTimeout(Promise.resolve(1), 100);
+    expect(promise).toBeInstanceOf(Promise);
+  });
+
+  describe('settled', () => {
+    describe('resolved', () => {
+      test('immediately', async () => {
+        const promise = promiseTimeout(Promise.resolve(42), 100);
+        await expect(promise).resolves.toBe(42);
+      });
+
+      test('next tick', async () => {
+        const promise = promiseTimeout(
+          new Promise((resolve) => {
+            setTimeout(() => {
+              resolve(42);
+            }, 0);
+          }),
+          100
+        );
+        await expect(promise).resolves.toBe(42);
+      });
+
+      test('before timeout', async () => {
+        const promise = promiseTimeout(
+          new Promise((resolve) => {
+            setTimeout(() => {
+              resolve(42);
+            }, 50);
+          }),
+          100
+        );
+        await expect(promise).resolves.toBe(42);
+      });
+    });
+
+    describe('rejected', () => {
+      test('immediately', async () => {
+        const promise = promiseTimeout(Promise.reject(42), 100);
+        await expect(promise).rejects.toBe(42);
+      });
+
+      test('next tick', async () => {
+        const promise = promiseTimeout(
+          new Promise((_, reject) => {
+            setTimeout(() => {
+              reject(42);
+            }, 0);
+          }),
+          100
+        );
+        await expect(promise).rejects.toBe(42);
+      });
+
+      test('before timeout', async () => {
+        const promise = promiseTimeout(
+          new Promise((_, reject) => {
+            setTimeout(() => {
+              reject(42);
+            }, 50);
+          }),
+          100
+        );
+        await expect(promise).rejects.toBe(42);
+      });
+    });
+  });
+
+  describe('timeout', () => {
+    test('immediate', async () => {
+      const promise = promiseTimeout(
+        new Promise((resolve) => {
+          setTimeout(() => {
+            resolve(42);
+          }, 500);
+        }),
+        0
+      );
+
+      await expect(promise).rejects.toBe('Promise timeout');
+    });
+
+    test('all immediate', async () => {
+      const promise = promiseTimeout(
+        new Promise((resolve) => {
+          setTimeout(() => {
+            resolve(42);
+          }, 0);
+        }),
+        0
+      );
+      await expect(promise).resolves.toBe(42);
+    });
+
+    test('non-immediate', async () => {
+      const promise = promiseTimeout(
+        new Promise((resolve) => {
+          setTimeout(() => {
+            resolve(42);
+          }, 200);
+        }),
+        100
+      );
+      await expect(promise).rejects.toBe('Promise timeout');
+    });
+  });
+});
